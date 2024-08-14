50be30606a25d7c4da78191bb3878cf6c9c405c4
GGON123 <jin0405@naver.com>
2024-08-14 18:42:58 +0900

    로그인페이지 버튼활성화 및 items페이지 전환 설정/ 회원가입페이지 수정중

--- login/index.js
+++ login/index.js
@@ -7,14 +7,17 @@ function validateEmail() {
 
   if (elInputEmail.value.length === 0) {
     blankEmailError.style.display = 'block';
+    elInputEmail.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     emailError.style.display = 'none';
     return false;
   } else if (elInputEmail.value.length > 0 && !emailPattern.test(elInputEmail.value)) {
     blankEmailError.style.display = 'none';
+    elInputEmail.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     emailError.style.display = 'block';
     return false;
   } else {
     blankEmailError.style.display = 'none';
+    elInputEmail.style.cssText = 'none';
     emailError.style.display = 'none';
     return true;
   }
@@ -28,22 +31,45 @@ function validatePassword() {
 
   if (elInputPassword.value.length === 0) {
     blankPasswordError.style.display = 'block';
+    elInputPassword.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     passwordError.style.display = 'none';
     return false;
   } else if (elInputPassword.value.length < 8) {
     blankPasswordError.style.display = 'none';
+    elInputPassword.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     passwordError.style.display = 'block';
     return false;
   } else {
     blankPasswordError.style.display = 'none';
+    elInputPassword.style.cssText = 'none';
     passwordError.style.display = 'none';
     return true;
   }
 }
 
+// 버튼 활성화
+function buttonActivation() {
+  const emailValid = validateEmail();
+  const passwordValid = validatePassword();
+  const loginButton = document.querySelector("#login-button");
+  let formValid = true;
+
+  if(emailValid && passwordValid) {
+    loginButton.disabled = !formValid;
+  } else {
+    loginButton.disabled = formValid;
+  }
+
+}
+
+//로그인버튼 클릭시 items페이지로 이동
+function goUrl() {
+  location.href = '/items';
+}
+
 document.querySelector('#email').addEventListener('focusout',validateEmail);// 이메일 입력상태 호출
 document.querySelector('#password').addEventListener('focusout',validatePassword); // 비밀번호 입력상태 호출
-
-
+document.querySelector('#password').addEventListener('focusout',buttonActivation);
+document.querySelector('#login-button').addEventListener('click',goUrl);
 
 
--- signup/index.js
+++ signup/index.js
@@ -7,14 +7,17 @@ function validateEmail() {
 
   if (elInputEmail.value.length === 0) {
     blankEmailError.style.display = 'block';
+    elInputEmail.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     emailError.style.display = 'none';
     return false;
   } else if (elInputEmail.value.length > 0 && !emailPattern.test(elInputEmail.value)) {
     blankEmailError.style.display = 'none';
+    elInputEmail.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     emailError.style.display = 'block';
     return false;
   } else {
     blankEmailError.style.display = 'none';
+    elInputEmail.style.cssText = 'none';
     emailError.style.display = 'none';
     return true;
   }
@@ -28,14 +31,17 @@ function validatePassword() {
 
   if (elInputPassword.value.length === 0) {
     blankPasswordError.style.display = 'block';
+    elInputPassword.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     passwordError.style.display = 'none';
     return false;
   } else if (elInputPassword.value.length < 8) {
     blankPasswordError.style.display = 'none';
+    elInputPassword.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     passwordError.style.display = 'block';
     return false;
   } else {
     blankPasswordError.style.display = 'none';
+    elInputPassword.style.cssText = 'nane;';
     passwordError.style.display = 'none';
     return true;
   }
@@ -50,22 +56,46 @@ function validateConfirmPassword() {
   
   if (elInputConfirmPassword.value.length === 0) {
     rewritePasswordError.style.display = 'block';
+    elInputConfirmPassword.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     confirmPasswordError.style.display = 'none';
     return false
   } else if (elInputPassword.value !== elInputConfirmPassword.value) {
     rewritePasswordError.style.display = 'none';
+    elInputConfirmPassword.style.cssText = 'outline: 0.1rem solid var(--error-color);';
     confirmPasswordError.style.display = 'block';
     return false;
   } else {
     confirmPasswordError.style.display = 'none';
+    elInputConfirmPassword.style.cssText = 'none';
     rewritePasswordError.style.display = 'none';
     return true;
   }
 }
 
+// 버튼 활성화
+function buttonActivation() {
+  const emailValid = validateEmail();
+  const passwordValid = validatePassword();
+  const confirmPasswordValid = validateConfirmPassword();
+  const signupButton = document.querySelector('#signup-button');
+  let formValid = true;
+
+  if(emailValid && passwordValid && confirmPasswordValid) {
+    signupButton.disabled = !formValid;
+  } else {
+    signupButton.disabled = formValid;
+  }
+}
+
+// 회원가입버튼 클릭시 login페이지로 이동
+function goUrl() {
+  location.href = '/login';
+}
+
 document.querySelector('#email').addEventListener('focusout',validateEmail);// 이메일 입력상태 호출
 document.querySelector('#password').addEventListener('focusout',validatePassword); // 비밀번호 입력상태 호출
 document.querySelector('#confirm-password').addEventListener('focusout',validateConfirmPassword);// 비밀번호확인 입력상태 호출
+document.querySelector('#signup-button').addEventListener('click',goUrl);
 
 
 
