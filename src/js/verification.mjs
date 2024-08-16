70e848e147e515ce42b043ad3bba1cdf9a75640b
csbizz <cheshirebizz@gmail.com>
2024-08-16 15:57:08 +0900

    Sprint3 Basic Req

---
+++ src/js/verification.mjs
@@ -0,0 +1,25 @@
+const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+
+const USER_DATA = [
+    { email: 'codeit1@codeit.com', password: "codeit101!" },
+    { email: 'codeit2@codeit.com', password: "codeit202!" },
+    { email: 'codeit3@codeit.com', password: "codeit303!" },
+    { email: 'codeit4@codeit.com', password: "codeit404!" },
+    { email: 'codeit5@codeit.com', password: "codeit505!" },
+    { email: 'codeit6@codeit.com', password: "codeit606!" },
+];
+
+function verifyId(email) {
+    return pattern.test(email);
+}
+
+function verifyPw(email, pw) {
+    // console.log(email, pw)
+    for(const usr of USER_DATA) {
+        if(usr.email === email && usr.password === pw)
+            return true;
+    }
+    return false;
+}
+
+export { verifyId, verifyPw };
