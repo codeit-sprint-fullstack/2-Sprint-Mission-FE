8e9f67c2813329362bed9b1de9d7828878f7c99d
annylee0723 <annylee0723@gmail.com>
2024-08-18 20:45:30 +0900

    compact functions

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
+const loginBtn = document.querySelector('.login-button');
+const emailBox = document.querySelector('#email-box');
+const emailInput = document.querySelector('#email');
+const passwordBox = document.querySelector('#password-box');
+const passwordInput = document.querySelector('#password')
+const toggleIcon = document.querySelector('#toggle-icon');
+const pwInvisible = document.querySelector('.vis-password');
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 const USER_DATA = [
     { email: 'codeit1@codeit.com', password: "codeit101!" },
     { email: 'codeit2@codeit.com', password: "codeit202!" },
@@ -7,117 +16,94 @@ const USER_DATA = [
     { email: 'codeit6@codeit.com', password: "codeit606!" },
 ];
 
-
-const emailBox = document.querySelector('#email-input');
-emailBox.addEventListener('focusout', (e) => {
-    const userEmail = document.querySelector('#email').value;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    // 기존의 에러 메시지를 모두 제거
-    const emailErrorNoInput = document.querySelector('#email-error-noinput');
-    const emailErrorWrongFormat = document.querySelector('#email-error-wrongformat');
-
-    if (emailErrorNoInput) emailErrorNoInput.remove();
-    if (emailErrorWrongFormat) emailErrorWrongFormat.remove();
-
-    // 값이 없을 경우
-    if (userEmail === "") {
-        emailBox.classList.add('gray-box-outer');
-        if (!document.querySelector('#email-error-noinput')) {
-            const msg = document.createElement("div");
-            msg.id = 'email-error-noinput';
-            msg.textContent = "이메일을 입력해주세요.";
-            msg.className = 'errorRed';
-            document.querySelector('#email-area').appendChild(msg);
-        }
-    }
-    // 이메일 형식에 맞지 않을 경우
-    else if (!emailRegex.test(userEmail)) {
-        emailBox.classList.add('gray-box-outer');
-        if (!document.querySelector('#email-error-wrongformat')) {
-            const msg = document.createElement("div");
-            msg.id = 'email-error-wrongformat';
-            msg.textContent = "잘못된 이메일 형식입니다.";
-            msg.className = 'errorRed';
-            document.querySelector('#email-area').appendChild(msg);
-        }
-    }
-    // 에러 없음: 스타일 제거 및 에러 메시지 제거
-    else {
-        emailBox.classList.remove('gray-box-outer');
-    }
-});
-
-
-const passwordBox = document.querySelector('#password-input');
-passwordBox.addEventListener('focusout', (e) => {
-    const userPassword = document.querySelector('#password').value;
-
-    //기존 에러 메세지 제거
-    const passwordInput = document.querySelector('#password-error-noinput');
-    const passwordErrorDigit = document.querySelector("#password-error-digit");
-
-    if (passwordInput) passwordInput.remove();
-    if (passwordErrorDigit) passwordErrorDigit.remove();
-
-    //비밀번호가 빈 값일 경우
-    if (userPassword === ""){
-        passwordBox.classList.add('gray-box-outer');
-        if (!document.querySelector('#password-error-noinput')) {
-            const msg = document.createElement("div");
-            msg.id = 'password-error-noinput';
-            msg.textContent = "비밀번호를 입력해주세요.";
-            msg.className = 'errorRed';
-            document.querySelector('#password-area').appendChild(msg);
-        }
-    }
-    //비밀번호가 8자 미만일 경우
-    else if (userPassword.length < 8){
-        passwordBox.classList.add('gray-box-outer');
-        if (!document.querySelector('#password-error-digit')) {
-            const msg = document.createElement("div");
-            msg.id = 'password-error-digit';
-            msg.textContent = '비밀번호를 8자 이상 입력해주세요.';
-            msg.className = 'errorRed';
-            document.querySelector('#password-area').appendChild(msg);
-        }
-    }
-    else{
-        passwordBox.classList.remove('gray-box-outer');
-    }
-});
-
-const loginBtn = document.querySelector('.login-button');
-
-// 버튼 활성화
-emailBox.addEventListener('keyup', validateInputs);
-passwordBox.addEventListener('keyup', validateInputs);
-
-// 입력값의 유효성을 검사 & 버튼 활성화
+// 입력값의 유효성을 검사 & 로그인 버튼 활성화
 function validateInputs() {
-    const userEmail = document.querySelector('#email').value;
-    const userPassword = document.querySelector('#password').value;
+    const userEmail = emailInput.value;
+    const userPassword = passwordInput.value;
     
     // 입력값 유효성 검사
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const isEmailValid = emailRegex.test(userEmail);
     const isPasswordValid = userPassword.length >= 8;
 
     // 유효성에 따라 로그인 버튼 활성화
-    if (isEmailValid && isPasswordValid) {
-        loginBtn.disabled = false; //
-    } else {
-        loginBtn.disabled = true; //
-    }
+    loginBtn.disabled = !(isEmailValid && isPasswordValid);
 };
 
+function loginFocusOut() {
+    const userEmail = emailInput.value;
+
+    //기존 error 삭제
+    removeErrorMessage('email-error-noinput');
+    removeErrorMessage('email-error-wrongformat');
+
+    // 값이 없을 경우
+    if (userEmail === "") {
+        emailBox.classList.add('gray-box-outer');
+        const msg = createErrorMsg('email-error-noinput', '이메일을 입력해주세요.');
+        document.querySelector('#email-area').appendChild(msg);
+    }
+    // 이메일 형식에 맞지 않을 경우
+    else if (!emailRegex.test(userEmail)) {
+        emailBox.classList.add('gray-box-outer');
+        const msg = createErrorMsg('email-error-wrongformat', '잘못된 이메일 형식입니다.');
+        document.querySelector('#email-area').appendChild(msg);
+    }
+    // 에러 없음: 스타일 제거
+    else {
+        emailBox.classList.remove('gray-box-outer');
+    }
+}
+
+function passwordFocusOut() {
+    const userPassword = passwordInput.value;
+
+    //기존 error 삭제
+    removeErrorMessage('password-error-noinput');
+    removeErrorMessage('password-error-wrongformat');
+
+    // 값이 없을 경우
+    if (userPassword === "") {
+        passwordBox.classList.add('gray-box-outer');
+        const msg = createErrorMsg('password-error-noinput', '비밀번호를 입력해주세요');
+        document.querySelector('#password-area').appendChild(msg);
+    }
+    // 비밀번호가 8자 이상인 경우
+    else if (userPassword.length < 8) {
+        passwordBox.classList.add('gray-box-outer');
+        const msg = createErrorMsg('password-error-wrongformat', '비밀번호를 8자 이상 입력해주세요.');
+        document.querySelector('#password-area').appendChild(msg);
+    }
+    // 에러 없음: 스타일 제거
+    else {
+        passwordBox.classList.remove('gray-box-outer');
+    }
+}
+
+// 에러 메시지 제거 함수
+function removeErrorMessage(id) {
+    const errorMessage = document.querySelector(`#${id}`);
+    if (errorMessage) {
+        errorMessage.remove();
+    }
+}
+
+// 에러 메시지 생성 함수
+function createErrorMsg(id, text) {
+    const msg = document.createElement("div");
+    msg.id = id;
+    msg.textContent = text;
+    msg.className = 'error-red';
+    return msg;
+}
+
 // 로그인 버튼 성공, 실패 여부
-loginBtn.addEventListener('click', (e) => {
-    const userEmail = document.querySelector('#email').value;
-    const userPassword = document.querySelector('#password').value;
+function loginResult () {
+    const userEmail = emailInput.value;
+    const userPassword = passwordInput.value;
+
     if (!loginBtn.disabled) {
         let loginSuccess = false;
-
+        //로그인 성공
         for (const user of USER_DATA) {
             if (userEmail === user.email && userPassword === user.password) {
                 alert('로그인 성공.');
@@ -126,23 +112,27 @@ loginBtn.addEventListener('click', (e) => {
                 break;
             }
         }
+        //로그인 실패
         if (!loginSuccess) {
             alert('비밀번호가 일치하지 않습니다.');
         }
     }
-});
+}
 
 //눈 아이콘 토글
-const userPassword = document.querySelector('#password')
-const pwInvisible = document.querySelector('.vis-password');
-const toggleIcon = document.querySelector('#toggle-icon');
-pwInvisible.addEventListener('click', (e) => {
-    if (userPassword.type === 'password') {
-        userPassword.type = 'text';
-        toggleIcon.src ='login_create_account_asset/btn_visibility_on_24px.svg';
+function passwordIconVis () {
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.src = 'login_create_account_asset/btn_visibility_on_24px.svg';
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.src = 'login_create_account_asset/btn_visibility_off_24px.svg';
     }
-    else{
-        userPassword.type = 'password'
-        toggleIcon.src ='login_create_account_asset/btn_visibility_off_24px.svg';
-    }
-})
+}
+
+emailBox.addEventListener('keyup', validateInputs);
+passwordBox.addEventListener('keyup', validateInputs);
+emailBox.addEventListener('focusout', loginFocusOut);
+passwordBox.addEventListener('focusout', passwordFocusOut);
+loginBtn.addEventListener('click', loginResult);
+pwInvisible.addEventListener('click', passwordIconVis);
