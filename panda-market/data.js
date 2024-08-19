fc2b75047256b543079719a37adac4bde63a4142
kipid <kipacti@gmail.com>
2024-08-20 07:33:04 +0900

    Update data.js
    Array to JSON

--- panda-market/data.js
+++ panda-market/data.js
@@ -1,8 +1,8 @@
-const USER_DATA = [
-	{ email: 'codeit1@codeit.com', password: "codeit101!" },
-	{ email: 'codeit2@codeit.com', password: "codeit202!" },
-	{ email: 'codeit3@codeit.com', password: "codeit303!" },
-	{ email: 'codeit4@codeit.com', password: "codeit404!" },
-	{ email: 'codeit5@codeit.com', password: "codeit505!" },
-	{ email: 'codeit6@codeit.com', password: "codeit606!" },
-];
+const USER_DATA = {
+	'codeit1@codeit.com': { email: 'codeit1@codeit.com', password: "codeit101!" },
+	'codeit2@codeit.com': { email: 'codeit2@codeit.com', password: "codeit202!" },
+	'codeit3@codeit.com': { email: 'codeit3@codeit.com', password: "codeit303!" },
+	'codeit4@codeit.com': { email: 'codeit4@codeit.com', password: "codeit404!" },
+	'codeit5@codeit.com': { email: 'codeit5@codeit.com', password: "codeit505!" },
+	'codeit6@codeit.com': { email: 'codeit6@codeit.com', password: "codeit606!" },
+};
