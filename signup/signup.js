2a42744b3d963e578397a47296b97ec20a8ed303
Seo-Yurim <yurim2204@naver.com>
2024-08-19 18:06:20 +0900

    Refactor: signup.js 함수 이름 수정

--- signup/signup.js
+++ signup/signup.js
@@ -66,8 +66,9 @@ function isPasswordValid(password) {
   }
 }
 
-function noneNameChecker() {
-  if (inputName.value !== '') {
+// 닉네임 유효성 검사 함수
+function noneNameChecker(name) {
+  if (name !== '') {
     noneName.classList.add('hide');
     inputName.classList.remove('error');
   } else {
@@ -76,35 +77,35 @@ function noneNameChecker() {
   }
 }
 
-function nonePasswordRepeatChecker() {
-  if (inputPasswordRepeat.value !== '') {
+// 비밀번호 재확인 유효성 검사 함수
+function isPasswordRepeatValid(password, rePassword) {
+  if (rePassword !== '') {
     nonePasswordRepeat.classList.add('hide');
     inputPasswordRepeat.classList.remove('error');
+    missMatchPassword.classList.add('hide');
+
+    if (password !== rePassword) {
+      missMatchPassword.classList.remove('hide');
+      inputPasswordRepeat.classList.add('error');
+      nonePasswordRepeat.classList.add('hide');
+    }
   } else {
     nonePasswordRepeat.classList.remove('hide');
     inputPasswordRepeat.classList.add('error');
-  }
-}
-
-function matchPassword() {
-  if (inputPassword.value === inputPasswordRepeat.value) {
     missMatchPassword.classList.add('hide');
-    inputPasswordRepeat.classList.remove('error');
-  } else {
-    missMatchPassword.classList.remove('hide');
-    inputPasswordRepeat.classList.add('error');
   }
 }
 
-function signupChecker() {
+// 회원가입 버튼 활성화 함수
+function signupBtnActivation(email, password, name, rePassword) {
   if (
-    inputEmail.value &&
-    inputPassword.value &&
-    inputName.value &&
-    inputPasswordRepeat.value &&
-    emailFormat.test(inputEmail.value) &&
-    inputPassword.value.length >= 8 &&
-    inputPassword.value === inputPasswordRepeat.value
+    email &&
+    password &&
+    name &&
+    rePassword &&
+    emailFormat.test(email) &&
+    password.length >= 8 &&
+    password === rePassword
   ) {
     signupBtn.classList.add('button-abled');
     signupBtn.disabled = false;
@@ -115,6 +116,7 @@ function signupChecker() {
   }
 }
 
+// 중복 이메일 확인 함수
 function isSignupValid(email) {
   const emailMatch = USER_DATA.find((value) => value.email === email);
 
@@ -132,20 +134,54 @@ function closeModal() {
   errorModal.classList.add('hide');
 }
 
-inputEmail.addEventListener('keyup', signupChecker);
-inputPassword.addEventListener('keyup', signupChecker);
-inputName.addEventListener('keyup', signupChecker);
-inputPasswordRepeat.addEventListener('keyup', signupChecker);
+// 회원가입 버튼 활성화 이벤트 생성
+inputEmail.addEventListener('keyup', () =>
+  signupBtnActivation(
+    inputEmail.value,
+    inputPassword.value,
+    inputName.value,
+    inputPasswordRepeat.value
+  )
+);
+inputPassword.addEventListener(
+  'keyup',
+  signupBtnActivation(
+    inputEmail.value,
+    inputPassword.value,
+    inputName.value,
+    inputPasswordRepeat.value
+  )
+);
+inputName.addEventListener(
+  'keyup',
+  signupBtnActivation(
+    inputEmail.value,
+    inputPassword.value,
+    inputName.value,
+    inputPasswordRepeat.value
+  )
+);
+inputPasswordRepeat.addEventListener(
+  'keyup',
+  signupBtnActivation(
+    inputEmail.value,
+    inputPassword.value,
+    inputName.value,
+    inputPasswordRepeat.value
+  )
+);
 
+// 포커스 아웃 시 유효성 검증 이벤트 생성
 inputEmail.addEventListener('focusout', () => isEmailValid(inputEmail.value));
 inputPassword.addEventListener('focusout', () =>
   isPasswordValid(inputPassword.value)
 );
+inputName.addEventListener('focusout', () => noneNameChecker(inputName.value));
+inputPasswordRepeat.addEventListener('focusout', () =>
+  isPasswordRepeatValid(inputPassword.value, inputPasswordRepeat.value)
+);
 
-inputName.addEventListener('focusout', noneNameChecker);
-inputPasswordRepeat.addEventListener('focusout', nonePasswordRepeatChecker);
-inputPasswordRepeat.addEventListener('focusout', matchPassword);
-
+// 중복 이메일 확인 이벤트 생성
 signupBtn.addEventListener('click', () => isSignupValid(inputEmail.value));
 errorOverlay.addEventListener('click', closeModal);
 errorOkBtn.addEventListener('click', closeModal);
