1e3c74592548ec961c4aac2eaa5cfb3eaa16315c
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-08-20 16:34:51 +0900

    8.20 database and landingpage additional update

--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-    //변수 선언
+document.addEventListener("DOMContentLoaded", function() {
     const inputEmail = document.getElementById("email");
     const inputPassword = document.getElementById("password");
-    const inputPasswordCheck = document.getElementById("passwordcheck");
 
     const errorEmail = document.getElementById("errorEmail");
     const errorPassword = document.getElementById("errorPassword");
-    const errorPasswordCheck = document.getElementById("errorPasswordCheck");
 
-    const signupBtn = document.querySelector(".signup-btn");
+    const loginBtn = document.querySelector(".login-btn");
 
-    //email 관련 함수 
-    /* 1. 아무 것도 입력하지 않았을 때
-       2. 잘못된 이메일 형식일 때
-       3. 제대로 입력했을 때*/
+    //8.20 데이터베이스 추가
+    const USER_DATA = [
+            { email: 'codeit1@codeit.com', password: "codeit101!" },
+            { email: 'codeit2@codeit.com', password: "codeit202!" },
+            { email: 'codeit3@codeit.com', password: "codeit303!" },
+            { email: 'codeit4@codeit.com', password: "codeit404!" },
+            { email: 'codeit5@codeit.com', password: "codeit505!" },
+            { email: 'codeit6@codeit.com', password: "codeit606!" },
+    ]
 
-    function validationEmail(){
+    //email 관련 함수
+
+    function validateEmail(){
         const emailValue = inputEmail.value.trim();
         if(!emailValue){
             errorEmail.textContent = "이메일을 입력해주세요.";
@@ -43,11 +46,8 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     //password 관련 함수
-    /* 1. 아무 것도 입력하지 않았을 때
-       2. 8자 이상 입력하지 않았을 때
-       3. 제대로 입력했을 때*/
 
-    function validationPassword(){
+    function validatePassword(){
         const passwordValue = inputPassword.value.trim();
 
         if(!passwordValue){
@@ -74,62 +74,54 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
-    //passwordcheck 관련 함수
-    /* 1. 비밀번호가 같지 않았을 때
-       2. 제대로 입력했을 때 */
-
-    function validationPasswordCheck(){
-        const passwordValue = inputPassword.value.trim();
-        const passwordcheckValue = inputPasswordCheck.value.trim();
-
-        if(passwordValue !== passwordcheckValue) {
-            errorPasswordCheck.textContent = "비밀번호가 일치하지 않습니다.";
-            inputPasswordCheck.classList.add("input-error-box");
-            errorPasswordCheck.style.display = "block";
-            
-            return false;
-        }
-
-        else{
-            errorPasswordCheck.style.display = "none";
-            inputPasswordCheck.classList.remove("input-error-box");
-
-            return true;
-        }
-
-    }
-
     //모든 요소들 체크
-    function ValidCheckInput(){
-        const isValidEmail = validationEmail();
-        const isValidPassword = validationPassword();
-        const isValidPasswordCheck = validationPasswordCheck();
+    function validCheckInput(){
+        const isValidEmail = validateEmail();
+        const isValidPassword = validatePassword();
         
-        if(isValidEmail && isValidPassword && isValidPasswordCheck){ // 버튼 색상
-            signupBtn.disabled = false;
-            signupBtn.style.backgroundColor = "#3692FF";
+        if(isValidEmail && isValidPassword){ // 버튼 색상
+            loginBtn.disabled = false;
+            loginBtn.style.backgroundColor = "#3692FF";
         }
         else {
-            signupBtn.disabled = true;
-            signupBtn.style.backgroundColor = "#9CA3AF";
-        };
+            loginBtn.disabled = true;
+            loginBtn.style.backgroundColor = "#9CA3AF";
+        }
     }
 
     //focus out
 
-    inputEmail.addEventListener("focusout", validationEmail);
-    inputPassword.addEventListener("focusout", validationPassword);
-    inputPasswordCheck.addEventListener("focusout", validationPasswordCheck);
+    inputEmail.addEventListener("focusout", validateEmail);
+    inputPassword.addEventListener("focusout", validatePassword);
 
-    inputEmail.addEventListener("input", ValidCheckInput);
-    inputPassword.addEventListener("input", ValidCheckInput);
-    inputPasswordCheck.addEventListener("input", ValidCheckInput);
+    inputEmail.addEventListener("input", validCheckInput);
+    inputPassword.addEventListener("input", validCheckInput);
 
-    //로그인 페이지로 이동(임시용)
-    signupBtn.addEventListener("click", function(){
-        if(!signupBtn.disabled){
-            window.location.href = "login.html";
+    //item 페이지로 이동
+    loginBtn.addEventListener("click", function(){
+        if(!loginBtn.disabled) {
+            window.location.href = "items.html";
         }
     });
+
+    //데이터베이스 로그인 버튼
+    loginBtn.addEventListener("click", function(){
+        if(!loginBtn.disabled){ //활성화될 때
+            const email = inputEmail.value.trim();
+            const password = inputPassword.value.trim();
+
+            if(email in USER_DATA){
+                if(USER_DATA[email] === password){ //비밀번호 일치 시
+                    window.location.href = "item.html"
+                }
+                else {
+                    alert("비밀번호가 일치하지 않습니다.");
+                }
+            }
+            else {
+                alert("입력한 이메일이 존재하지 않습니다.");
+            }
+        }
+    })
 
 });
