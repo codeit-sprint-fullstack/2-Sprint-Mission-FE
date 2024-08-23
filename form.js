7ceacc8d06315e315a7d336be61ec6c143171c54
Jaewon Kim <magry78@gmail.com>
2024-08-23 11:31:33 +0900

    기본 공통 로그인 부분 js 완성

--- form.js
+++ form.js
@@ -7,12 +7,14 @@
 
  input 에 빈 값이 있거나 에러 메세지가 있으면 ‘로그인’ 버튼은 비활성화 됩니다.
  Input 에 유효한 값을 입력하면 ‘로그인' 버튼이 활성화 됩니다.
+ 
  활성화된 ‘로그인’ 버튼을 누르면 “/items” 로 이동합니다
   */
 // 요소들을 가져오기
 const emailInput = document.querySelector('input[type="email"]');
 const passwordInput = document.querySelector('input[type="password"]');
 const loginButton = document.querySelector('#login');
+const loginForm = document.querySelector('login');
 
 const emailError = document.querySelector('#emailError');
 const passwordError = document.querySelector('#passwordError');
@@ -35,14 +37,16 @@ const USER_DATA = [
 const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
 function validateEmail(email) {
   if (email === "") {
+      emailInput.style.border = "1px solid #F74747";
       emailInput.classList.add('error');
       emailError.innerHTML = "이메일을 입력해주세요.";
   } else if (!emailPattern.test(email)) {
+      emailInput.style.border = "1px solid #F74747";
       emailInput.classList.add('error');
       emailError.innerHTML = "잘못된 이메일 형식입니다.";
   } else {
+      emailInput.style.border = "";
       emailInput.classList.remove('error');
-      console.log(emailInput.classList);
       emailError.innerHTML = "";
   }
 }
@@ -50,46 +54,19 @@ function validateEmail(email) {
 // 비밀번호 유효성 검사
 function validatePassword(password) {
   if (password === "") {
+      passwordInput.style.border = "1px solid #F74747";
       passwordInput.classList.add('error');
       passwordError.innerHTML = "비밀번호를 입력해주세요.";
   } else if (password.length < 8) {
+      passwordInput.style.border = "1px solid #F74747";
       passwordInput.classList.add('error');
       passwordError.innerHTML = "비밀번호를 8자 이상 입력해주세요.";
   } else {
+      passwordInput.style.border = "";
       passwordInput.classList.remove('error');
       passwordError.innerHTML = "";
   }
 }
-
-// 이벤트 리스너 설정
-// 이벤트 리스너에 전달되는 함수는 참조만 해야 하며, 실행되지 않아야 합니다
-emailInput.addEventListener('focusout', () => validateEmail(emailInput.value));
-passwordInput.addEventListener('focusout', () => validatePassword(passwordInput.value));
-
-// 로그인 버튼 초기화 비활성화
-loginButton.disabled = true;
-
-// 검증 후 로그인 버튼 다루기
-function validateForm(email, password) {
-  const emailIsValid = !emailInput.classList.contains('error') && (email !== "");
-  const passwordIsValid = !passwordInput.classList.contains('error') && (password !== "") && password.length >= 8;
-  console.log(passwordIsValid, emailIsValid);
-  console.log(validateEmail(emailInput.value));
-
-  // 폼 전체 검증을 통해 로그인 버튼 활성화 또는 비활성화
-  if (emailIsValid && passwordIsValid) {
-      loginButton.disabled = false;
-      loginButton.classList.add('button-click');
-  } else {
-      loginButton.disabled = true;
-      loginButton.classList.remove('button-click');
-  }
-}
-
-// 로그인 버튼 활성화 이벤트 생성
-emailInput.addEventListener('input', () => validateForm(emailInput.value, passwordInput.value));
-passwordInput.addEventListener('input', () => validateForm(emailInput.value, passwordInput.value));
-
 
 // 비밀번호 표시/숨기기 설정
 hideEye.addEventListener('click', showPassword);
@@ -114,15 +91,16 @@ function hidePassword() {
 
 /*
 // 폼 제출 이벤트 처리
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.querySelector('login').addEventListener('submit', function(event) {
   event.preventDefault(); // 기본 폼 제출 동작 막기
 
   // 로그인 버튼이 활성화된 상태에서만 /items로 이동
   if (!loginButton.disabled) {
-      window.location.href = "/items";
+      window.location.href = "../items/index.html";
   }
 });
-
+*/
+/*
 // 비밀번호 일치 여부를 확인하는 함수
 function isMatch (password1, password2) {
   return password1 === password2;
---
+++ login/login.js
@@ -0,0 +1,39 @@
+// 이벤트 리스너 설정
+// 이벤트 리스너에 전달되는 함수는 참조만 해야 하며, 실행되지 않아야 합니다
+emailInput.addEventListener('focusout', () => {
+  validateEmail(emailInput.value);
+  validateForm();
+});
+passwordInput.addEventListener('focusout', () => {
+  validatePassword(passwordInput.value);
+  validateForm();
+});
+
+// 로그인 버튼 초기화 비활성화
+loginButton.disabled = true;
+
+// 검증 후 로그인 버튼 다루기
+function validateForm() {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  const emailIsValid = !emailInput.classList.contains('error') && (email !== "");
+  const passwordIsValid = !passwordInput.classList.contains('error') && (password !== "") && password.length >= 8;
+
+  // 폼 전체 검증을 통해 로그인 버튼 활성화 또는 비활성화
+  if (emailIsValid && passwordIsValid) {
+    loginButton.disabled = false;
+    loginButton.classList.add('button-click');
+  } else {
+    loginButton.disabled = true;
+    loginButton.classList.remove('button-click');
+  }
+}
+
+// 로그인 버튼 활성화 이벤트 생성
+loginButton.addEventListener('click', () => {
+  let link = '../items/index.html';
+  if (!loginButton.disabled) {
+    window.location.href = link;
+  }
+});
