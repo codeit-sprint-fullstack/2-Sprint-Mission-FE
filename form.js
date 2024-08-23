f75cdb76acb50ee3e2bcbb97edcc127a0347893a
Jaewon Kim <magry78@gmail.com>
2024-08-23 15:46:48 +0900

    delete: USER_DATA in form.js

--- form.js
+++ form.js
@@ -16,17 +16,6 @@ const showEye = document.querySelector('#eye-view');
 const hideEye2 = document.querySelector('#eye-not2');
 const showEye2 = document.querySelector('#eye-view2');
 
-// 로그인/회원가입 유저 데이터
-const USER_DATA = [
-  { email: 'magry78@gmail.com', password: "galaxy5540*" },
-  { email: 'codeit1@codeit.com', password: "codeit101!" },
-  { email: 'codeit2@codeit.com', password: "codeit202!" },
-  { email: 'codeit3@codeit.com', password: "codeit303!" },
-  { email: 'codeit4@codeit.com', password: "codeit404!" },
-  { email: 'codeit5@codeit.com', password: "codeit505!" },
-  { email: 'codeit6@codeit.com', password: "codeit606!" },
-];
-
 // 이메일 유효성 검사
 const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
 function validateEmail(email) {
