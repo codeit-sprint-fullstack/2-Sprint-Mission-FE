c55c00b6e54b5e43398ef0f770a289556ceb2695
csbizz <cheshirebizz@gmail.com>
2024-08-23 13:54:18 +0900

    set Tablet & Mobile layout

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,9 +1,14 @@
 const topH1 = document.getElementById('topBannerTitle');
+const logo = document.getElementById('pandaLogo');
 
 if (window.innerWidth < 1200 && window.innerWidth > 743) {
   topH1.innerHTML = `
             일상의 모든 물건을 거래해 보세요
           `;
+}
+
+if (window.innerWidth < 744) {
+  logo.src = 'src/Image/small_logo.png';
 }
 
 window.addEventListener('resize', (e) => {
@@ -16,5 +21,11 @@ window.addEventListener('resize', (e) => {
             일상의 모든 물건을<br>
             거래해 보세요
           `;
+  }
+
+  if (window.innerWidth < 744) {
+    logo.src = 'src/Image/small_logo.png';
+  } else {
+    logo.src = 'src/Image/logo.png';
   }
 });
