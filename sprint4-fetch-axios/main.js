4a743047820c74b1b7d46a0ec58dfd157f081ee5
kipid <kipacti@gmail.com>
2024-08-26 13:47:03 +0900

    Update main.js

--- sprint4-fetch-axios/main.js
+++ sprint4-fetch-axios/main.js
@@ -4,13 +4,13 @@ import product from "./ProductService.js";
 /////////////////////////////////////////////////////
 // * TEST code
 /////////////////////////////////////////////////////
-// console.log(await article.getArticleList());
-// console.log(await article.getArticle(81));
-// console.log(await article.createArticle({title: "string", content: "string", image: "string"}));
-// console.log(await article.patchArticle(258, {title: "new title", content: "new content", image: "new image"}));
+console.log(await article.getArticleList({page: 1, pageSize: 3}));
+console.log(await article.getArticle(81));
+console.log(await article.createArticle({title: "string", content: "string", image: "string"}));
+console.log(await article.patchArticle(258, {title: "new title", content: "new content", image: "new image"}));
 // console.log(await article.deleteArticle(257));
 
-// console.log(await product.getProductList());
+console.log(await product.getProductList({page: 1, pageSize: 3}));
 console.log(await product.getProduct(9));
 console.log(await product.createProduct({name: "name", description: "설명", price: 1000, tags: ["tags"], images: ["images"]}));
 console.log(await product.patchProduct(9, {name: "new name"}));
