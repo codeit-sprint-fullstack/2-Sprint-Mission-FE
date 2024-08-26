e7ddf920e7b4dc7c8d81ed6fb81ec99a7d08aefe
csbizz <cheshirebizz@gmail.com>
2024-08-26 20:37:59 +0900

    Abstract api services

--- src/js/ArticleService.js
+++ src/js/ArticleService.js
@@ -1,39 +1,79 @@
 const SERVER = 'https://sprint-mission-api.vercel.app/articles/';
 
-function jsonIfOK(res) {
-  // return res.ok ? res.json() : res;
-  if (res.ok) return res.json();
+async function fetchData(url, method, body = {}, params = {}) {
+  const server = new URL(url);
+  for (let key in params) {
+    server.searchParams.append(key, params[key]);
+  }
 
-  throw new Error(res.status + ' ' + res.statusText);
+  const res =
+    method === 'POST' || method === 'PATCH'
+      ? fetch(server, {
+          method,
+          body: JSON.stringify(body),
+          headers: { 'Content-Type': 'application/json' }
+        })
+      : fetch(server);
+
+  const data = await res
+    .then((r) => {
+      if (r.status === 204) return r.status;
+      else if (r.ok) return r.json();
+
+      throw new Error(r.status + ' ' + r.statusText);
+    })
+    .catch((e) => console.log(e.message));
+
+  // const res = await fetch(server, {
+  //   method,
+  //   body: JSON.stringify(body),
+  //   headers: { 'Content-Type': 'application/json' }
+  // })
+  //   .then((r) => {
+  //     if (r.ok) return r.json();
+
+  //     throw new Error(r.status + ' ' + r.statusText);
+  //   })
+  //   .catch((e) => console.log(e.message));
+  return data;
 }
 
+// function jsonIfOK(res) {
+//   // return res.ok ? res.json() : res;
+//   if (res.ok) return res.json();
+
+//   throw new Error(res.status + ' ' + res.statusText);
+// }
+
 async function getArticleList(params = {}) {
-  const url = new URL(SERVER);
-  for (let key in params) {
-    url.searchParams.append(key, params[key]);
-  }
+  // const url = new URL(SERVER);
+  // for (let key in params) {
+  //   url.searchParams.append(key, params[key]);
+  // }
 
   // const res = await fetch(url);
   // const data = await res.json();
 
-  return fetch(url)
-    .then((res) => jsonIfOK(res))
-    .catch((e) => console.log(e.message));
+  // return fetch(url)
+  //   .then((res) => jsonIfOK(res))
+  //   .catch((e) => console.log(e.message));
+  return fetchData(SERVER, 'GET', {}, params);
 }
 
 async function getArticle(id) {
-  const url = new URL(SERVER + id);
+  // const url = new URL(SERVER + id);
 
   // const res = await fetch(url);
   // const data = await res.json();
 
-  return fetch(url)
-    .then((res) => jsonIfOK(res))
-    .catch((e) => console.log(e.message));
+  // return fetch(url)
+  //   .then((res) => jsonIfOK(res))
+  //   .catch((e) => console.log(e.message));
+  return fetchData(SERVER + id, 'GET');
 }
 
 async function createArticle(article) {
-  const url = new URL(SERVER);
+  // const url = new URL(SERVER);
 
   // const res = await fetch(url, {
   //   method: 'POST',
@@ -42,17 +82,18 @@ async function createArticle(article) {
   // });
   // const data = await res.json();
 
-  return fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(article),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then((res) => jsonIfOK(res))
-    .catch((e) => console.log(e.message));
+  // return fetch(url, {
+  //   method: 'POST',
+  //   body: JSON.stringify(article),
+  //   headers: { 'Content-Type': 'application/json' }
+  // })
+  //   .then((res) => jsonIfOK(res))
+  //   .catch((e) => console.log(e.message));
+  return fetchData(SERVER, 'POST', article);
 }
 
 async function patchArticle(id, article) {
-  const url = new URL(SERVER + id);
+  // const url = new URL(SERVER + id);
 
   // const res = await fetch(url, {
   //   method: 'PATCH',
@@ -61,22 +102,24 @@ async function patchArticle(id, article) {
   // });
   // const data = await res.json();
 
-  return fetch(url, {
-    method: 'PATCH',
-    body: JSON.stringify(article),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then((res) => jsonIfOK(res))
-    .catch((e) => console.log(e.message));
+  // return fetch(url, {
+  //   method: 'PATCH',
+  //   body: JSON.stringify(article),
+  //   headers: { 'Content-Type': 'application/json' }
+  // })
+  //   .then((res) => jsonIfOK(res))
+  //   .catch((e) => console.log(e.message));
+  return fetchData(SERVER + id, 'PATCH', article);
 }
 
 async function deleteArticle(id) {
-  const url = new URL(SERVER + id);
+  // const url = new URL(SERVER + id);
 
-  const res = await fetch(url, {
-    method: 'DELETE'
-  });
-  return res.status;
+  // const res = await fetch(url, {
+  //   method: 'DELETE'
+  // });
+  // return res.status;
+  return fetchData(SERVER + id, 'DELETE');
 }
 
 export {
--- src/js/ProductService.js
+++ src/js/ProductService.js
@@ -5,58 +5,69 @@ const instance = axios.create({
   timeout: 5000
 });
 
-function isOK(res) {
-  return res.status >= 200 && res.status < 300;
-}
+instance.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    throw new Error(err.status + ' ' + err.statusText);
+  }
+);
 
-function returnIfOK(res) {
-  if (!isOK(res)) throw new Error(res.status + ' ' + res.statusText);
+// function isOK(res) {
+//   return res.status >= 200 && res.status < 300;
+// }
 
-  return res.data;
-}
+// function returnIfOK(res) {
+//   if (!isOK(res)) throw new Error(res.status + ' ' + res.statusText);
 
-function requestGet(url, params) {
+//   return res.data;
+// }
+
+function getRequest(url, params) {
   return instance.get(url, params);
 }
 
-function requestPost(url, body) {
+function postRequest(url, body) {
   return instance.post(url, body);
 }
 
-function requestPatch(url, body) {
+function patchRequest(url, body) {
   return instance.patch(url, body);
 }
 
-function requestDelete(url) {
+function deleteRequest(url) {
   return instance.delete(url);
 }
 
 async function getProductList(params = {}) {
-  const res = await requestGet('/', { params });
+  const res = await getRequest('/', { params });
 
-  return returnIfOK(res);
+  // return returnIfOK(res);
+  return res.data;
 }
 
 async function getProduct(id) {
-  const res = await requestGet('/' + id);
+  const res = await getRequest('/' + id);
 
-  return returnIfOK(res);
+  // return returnIfOK(res);
+  return res.data;
 }
 
 async function createProduct(product) {
-  const res = await requestPost('/', product);
+  const res = await postRequest('/', product);
 
-  return returnIfOK(res);
+  // return returnIfOK(res);
+  return res.data;
 }
 
 async function patchProduct(id, product) {
-  const res = await requestPatch('/' + id, product);
+  const res = await patchRequest('/' + id, product);
 
-  return returnIfOK(res);
+  // return returnIfOK(res);
+  return res.data;
 }
 
 async function deleteProduct(id) {
-  const res = await requestDelete('/' + id);
+  const res = await deleteRequest('/' + id);
 
   return res.status;
 }
