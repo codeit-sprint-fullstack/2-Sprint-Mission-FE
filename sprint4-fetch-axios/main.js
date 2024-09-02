afe64ef37147f49b1aa8c40385bd64cb3038cd2e
kipid <kipacti@gmail.com>
2024-09-03 05:05:15 +0900

    Update main.js
    renaming and uncomment delete...

--- sprint4-fetch-axios/main.js
+++ sprint4-fetch-axios/main.js
@@ -1,17 +1,17 @@
-import article from "./ArticleService.js";
-import product from "./ProductService.js";
+import articleService from "./ArticleService.js";
+import productService from "./ProductService.js";
 
 /////////////////////////////////////////////////////
 // * TEST code
 /////////////////////////////////////////////////////
-console.log(await article.getArticleList({page: 1, pageSize: 3}));
-console.log(await article.getArticle(81));
-console.log(await article.createArticle({title: "string", content: "string", image: "string"}));
-console.log(await article.patchArticle(258, {title: "new title", content: "new content", image: "new image"}));
-// console.log(await article.deleteArticle(257));
+console.log(await articleService.getArticleList({page: 1, pageSize: 3}));
+console.log(await articleService.getArticle(81));
+console.log(await articleService.createArticle({title: "string", content: "string", image: "string"}));
+console.log(await articleService.patchArticle(258, {title: "new title", content: "new content", image: "new image"}));
+console.log(await articleService.deleteArticle(257));
 
-console.log(await product.getProductList({page: 1, pageSize: 3}));
-console.log(await product.getProduct(9));
-console.log(await product.createProduct({name: "name", description: "설명", price: 1000, tags: ["tags"], images: ["images"]}));
-console.log(await product.patchProduct(9, {name: "new name"}));
-// console.log(await product.deleteProduct(9));
+console.log(await productService.getProductList({page: 1, pageSize: 3}));
+console.log(await productService.getProduct(9));
+console.log(await productService.createProduct({name: "name", description: "설명", price: 1000, tags: ["tags"], images: ["images"]}));
+console.log(await productService.patchProduct(9, {name: "new name"}));
+console.log(await productService.deleteProduct(9));
