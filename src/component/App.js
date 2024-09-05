38d0faf874587c080dbcc175bea7a77d2ad66879
shinji530 <shinji53059@gmail.com>
2024-09-05 20:55:29 +0900

    Feat: 반응형 디자인 추가

--- src/component/App.js
+++ src/component/App.js
@@ -2,6 +2,7 @@ import { Fragment, useState, useEffect, useCallback } from "react";
 import "../css/App.css";
 import ic_arrow_down from '../assets/img/ic_arrow_down.png';
 import ic_search from '../assets/img/ic_search.png';
+import ic_sort from '../assets/img/ic_sort.png';
 import Footer from "./Footer";
 import Nav from "./Nav";
 import BestItem from "./BestItem";
@@ -17,16 +18,38 @@ function App() {
   const [isToggle, setIsToggle] = useState(false);
   const [currentPage, setCurrentPage] = useState(1); 
   const [totalCount, setTotalCount] = useState(1); 
+  const [pageSize, setPageSize] = useState(10);  
+  const [bestItemSize, setBestItemSize] = useState(4);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 743) {
+        setPageSize(4);
+        setBestItemSize(1);
+      } else if (window.innerWidth <= 1199) {
+        setPageSize(6);
+        setBestItemSize(2);
+      } else {
+        setPageSize(10);
+        setBestItemSize(4);
+      }
+    };
+
+    handleResize(); 
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   // BestItem 데이터만 한 번 로드
   useEffect(() => {
     const fetchBestItems = async () => {
-      const { list: productList } = await getProductList({ orderBy: "favorite" });
-      setBestItems(productList.sort((a, b) => b.favorite - a.favorite).slice(0, 4));
+      const { list: productList } = await getProductList({ orderBy: "favorite", pageSize: bestItemSize });
+      setBestItems(productList.sort((a, b) => b.favorite - a.favorite));
     };
 
     fetchBestItems();
-  }, []);
+  }, [bestItemSize]);
 
   const sortedProducts = products.sort((a, b) => a[order] - b[order]);
 
@@ -47,7 +70,7 @@ function App() {
   const toggleSortMenu = () => setIsToggle(!isToggle);
 
   const handleGetProductList = useCallback(async (orderQuery) => {
-    let queryParams = { orderBy: orderQuery, page: currentPage, pageSize: 10 };
+    let queryParams = { orderBy: orderQuery, page: currentPage, pageSize: pageSize };
 
     if (keyword) {
       queryParams.keyword = keyword;
@@ -57,7 +80,7 @@ function App() {
     setProducts(productList);
     setTotalCount(fetchedTotalCount);
     // console.log(productList);
-  }, [keyword, currentPage]);
+  }, [keyword, currentPage, pageSize]);
 
   useEffect(() => {
     handleGetProductList(order, currentPage);
@@ -90,8 +113,11 @@ function App() {
               <button className="item-register">상품 등록하기</button>
               <div className="sort-menu">
                 <button className="sort-toggle" onClick={toggleSortMenu}>
-                  {order === 'favorite' ? <p>좋아요순</p> : <p>최신순</p>}
-                  <img className='arrow' src={ic_arrow_down} alt='ic_arrow_down'></img>
+                  {pageSize === 4 ? <img src={ic_sort} alt='ic_sort'></img> : 
+                  <div className='sort-context'>
+                    {order === 'favorite' ? <p>좋아요순</p> : <p>최신순</p>}
+                    <img className='arrow' src={ic_arrow_down} alt='ic_arrow_down' />
+                  </div>}
                 </button>
                 {isToggle && (
                   <div className="sort-options">
