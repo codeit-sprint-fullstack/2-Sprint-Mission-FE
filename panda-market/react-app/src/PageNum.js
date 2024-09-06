f9baf36a26349ccb1c259c0b0b1fcfcf28b52fc3
kipid <kipacti@gmail.com>
2024-09-06 20:03:19 +0900

    Update PageNum.js

--- panda-market/react-app/src/PageNum.js
+++ panda-market/react-app/src/PageNum.js
@@ -1,9 +1,14 @@
 function PageNum({}) {
 	return (
-	<>
+	<div>
 		<div className="page-num">&lt;</div>
+		<div className="page-num">1</div>
+		<div className="page-num">2</div>
+		<div className="page-num">3</div>
+		<div className="page-num">4</div>
+		<div className="page-num">5</div>
 		<div className="page-num">&gt;</div>
-	</>
+	</div>
 	);
 }
 
