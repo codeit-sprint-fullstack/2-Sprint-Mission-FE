21e8a7e25f79150d8942d2513ef92b0c836f973e
kipid <kipacti@gmail.com>
2024-09-06 20:03:55 +0900

    Update itemsService.js

--- panda-market/react-app/src/apis/itemsService.js
+++ panda-market/react-app/src/apis/itemsService.js
@@ -4,44 +4,49 @@ const instance = axios.create({
   baseURL: `https://panda-market-api.vercel.app`
 });
 
-export async function getProducts({ page = 1, pageSize = 10, orderBy = "recent", keyword = "" }) {
-	const products = await instance.get(`/products`, { page, pageSize, orderBy, keyword });
-	const data = await products.json();
-	return data;
+async function getProducts(params = { page: 1, pageSize: 10, orderBy: "recent", keyword: "" }) {
+	const products = await instance.get(`/products`, {params});
+	return products;
 }
 
-export async function getProductWithId(id) {
+async function getProductWithId(id) {
 	const product = await instance.get(`/products/${id}`);
-	const data = await product.json();
-	return data;
+	return product;
 }
 
-export async function postProduct({ images = [], tags = [], price = 0, description = "", name = "" }) {
-	const resp = await instance.post(`/products`, {images, tags, price, description, name});
-	const data = await resp.json();
-	return data;
+async function postProduct(data = { images: [], tags: [], price: 0, description: "", name: "" }) {
+	const resp = await instance.post(`/products`, data);
+	return resp;
 }
 
-export async function patchProductWithId(id, {images, tags, price, description, name}) {
-	const resp = await instance.patch(`/products/${id}`, {images, tags, price, description, name});
-	const data = await resp.json();
-	return data;
+async function patchProductWithId(id, data = {images, tags, price, description, name}) {
+	const resp = await instance.patch(`/products/${id}`, data);
+	return resp;
 }
 
-export async function deleteProductWithId(id) {
+async function deleteProductWithId(id) {
 	const resp = await instance.delete(`/products/${id}`);
-	const data = await resp.json();
-	return data;
+	return resp;
 }
 
-export async function putProductToFavorite(id) {
+async function putProductToFavorite(id) {
 	const resp = await instance.post(`/products/${id}/favorite`);
-	const data = await resp.json();
-	return data;
+	return resp;
 }
 
-export async function deleteProductFromFavorite(id) {
+async function deleteProductFromFavorite(id) {
 	const resp = await instance.delete(`/products/${id}/favorite`);
-	const data = await resp.json();
-	return data;
+	return resp;
 }
+
+const itemsService = {
+	getProducts,
+	getProductWithId,
+	postProduct,
+	patchProductWithId,
+	deleteProductWithId,
+	putProductToFavorite,
+	deleteProductFromFavorite
+};
+
+export default itemsService;
