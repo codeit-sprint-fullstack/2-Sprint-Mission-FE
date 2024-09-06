315b9941cb22e542571242e4744069fb70e532a6
kipid <kipacti@gmail.com>
2024-09-06 20:03:12 +0900

    Update ItemsList.js

--- panda-market/react-app/src/ItemsList.js
+++ panda-market/react-app/src/ItemsList.js
@@ -7,8 +7,8 @@ function ItemsList({ items, orderBy, setOrderBy, keyword, setKeyword }) {
 				<h2>판매 중인 상품</h2>
 				<div>
 					<div className="input-wrapper">
-						<img src="/images/ic_search.svg" alt="Search"/>
 						<input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
+						<img src="/images/ic_search.svg" alt="Search"/>
 					</div>
 					<div className="post-product">상품 등록하기</div>
 					<select value={orderBy} onChange={(e) => setOrderBy(e.target.value)}>
