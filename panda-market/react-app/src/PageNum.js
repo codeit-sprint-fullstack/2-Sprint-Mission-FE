7ef50af67a933df7fc35b9b7aa6a4895d3a3662c
kipid <kipacti@gmail.com>
2024-09-07 12:51:50 +0900

    Update PageNum.js

--- panda-market/react-app/src/PageNum.js
+++ panda-market/react-app/src/PageNum.js
@@ -1,7 +1,39 @@
-function PageNum({}) {
+import { useEffect, useRef } from 'react';
+
+function PageNum({pageNum, setPageNum, pageNumMax}) {
 	return (
-	<div className="pagenation"><div className="disabled">&lt;</div><div className="selected">1</div><div>2</div><div>3</div><div>4</div><div>5</div><div>&gt;</div></div>
-	);
+	<div className="pagenation" onClick={function (event) {
+		const pages = event.currentTarget.querySelectorAll("div");
+			const target = event.target;
+			const targetN = Number(target.innerText);
+			if (isNaN(targetN)) {
+				if (!target.classList.contains("disabled")) {
+					if (target.innerText === "&lt;" || target.innerText === "<") {
+						setPageNum(Number(pages[1].innerText) - 1);
+					}
+					else if (target.innerText === "&gt;" || target.innerText === ">") {
+						setPageNum(Number(pages[pages.length - 2].innerText) + 1);
+					}
+				}
+			}
+			else {
+				setPageNum(targetN);
+			}
+	}}>
+		{(pageNum <= 1) ?
+		<><div className="disabled">&lt;</div><div className="selected">1</div>{pageNumMax >= 2 && <div>2</div>}{pageNumMax >= 3 && <div>3</div>}{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}<div className={pageNumMax <= 5 ? `disabled` : ""}>&gt;</div></>
+		:(pageNum <= pageNumMax && pageNum === 2) ?
+		<><div className="disabled">&lt;</div><div>1</div><div className="selected">2</div>{pageNumMax >= 3 && <div>3</div>}{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}<div className={pageNumMax <= 5 ? `disabled` : ""}>&gt;</div></>
+		:(pageNum <= pageNumMax && pageNum === 3) ?
+		<><div className="disabled">&lt;</div><div>1</div><div>2</div><div className="selected">3</div>{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}<div className={pageNumMax <= 5 ? `disabled` : ""}>&gt;</div></>
+		:(pageNum < pageNumMax - 2 && pageNum > 3) ?
+		<><div>&lt;</div><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className="selected">{pageN}</div><div>{pageNum + 1}</div><div>{pageNum + 2}</div><div>&gt;</div></>
+		:(pageNum === pageNumMax - 2) ?
+		<><div>&lt;</div><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className="selected">{pageN}</div><div>{pageNum + 1}</div><div>{pageNum + 2}</div><div className="disabled">&gt;</div></>
+		:(pageNum === pageNumMax - 1) ?
+		<><div>&lt;</div><div>{pageNum - 3}</div><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className="selected">{pageN}</div><div>{pageNum + 1}</div><div className="disabled">&gt;</div></>
+		:<><div>&lt;</div><div>{pageNumMax - 4}</div><div>{pageNumMax - 3}</div><div>{pageNumMax - 2}</div><div>{pageNumMax - 1}</div><div className="selected">{pageNumMax}</div><div className="disabled">&gt;</div></>}
+	</div>);
 }
 
 export default PageNum;
