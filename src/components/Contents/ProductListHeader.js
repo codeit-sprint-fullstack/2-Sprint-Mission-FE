3518362ae23dea09f21dfc215b829264818feaeb
Kimtaeyeon <gggm0117@naver.com>
2024-09-07 15:27:58 +0900

    refactor: remove SearchBox component and rename search-sort-box to layout-container

--- src/components/Contents/ProductListHeader.js
+++ src/components/Contents/ProductListHeader.js
@@ -1,15 +1,19 @@
 import CategoryTitle from "./CategoryTitle.js";
 import "./style/ProductListHeader.css";
-import SearchBox from "./SearchBox.js";
 import SortSelector from "./SortSelector.js";
 function ProductListHeader() {
   return (
     <div id="product-list-header">
+
       <CategoryTitle>판매 중인 상품</CategoryTitle>
-      <div className="layout-container">
-        <SearchBox id="search-box" />
+      <div id="search-sort-box">
+        <div id="search-box">
+          <input id="search-input"></input>
+          <button id="register-btn">상품 등록하기</button>
+        </div>
         <SortSelector />
       </div>
+
     </div>
   );
 }
