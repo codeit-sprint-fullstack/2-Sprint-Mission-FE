5980e0ac0abbf7a5450e62f62037cde7116f6186
kipid <kipacti@gmail.com>
2024-09-08 01:26:28 +0900

    Update ItemsList.js

--- panda-market/react-app/src/ItemsList.js
+++ panda-market/react-app/src/ItemsList.js
@@ -1,14 +1,19 @@
-import Item from "./Item";
+import Item from "./Item.js";
 
-function ItemsList({ items, orderBy, setOrderBy, keyword, setKeyword }) {
+function ItemsList({ items, orderBy, setOrderBy, keyword, setKeyword, onSearch }) {
 	return (
 		<section className="sec-items">
 			<div className="items-head">
 				{window.innerWidth > 744 ? <><h2>판매 중인 상품</h2>
 				<div className="items-head-query">
 					<div className="input-wrapper">
-						<input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
-						<img src="/images/ic_search.svg" alt="Search"/>
+						<input type="text" value={keyword} onKeyDown={(e) => {
+							if (e.code === "Enter") {
+								e.preventDefault();
+								onSearch();
+							}
+						}} onChange={(e) => setKeyword(e.target.value)}/>
+						<img src="/images/ic_search.svg" alt="Search" onClick={onSearch}/>
 					</div>
 					<div className="post-product">상품 등록하기</div>
 					<select className="select-order-by" value={orderBy} onChange={(e) => setOrderBy(e.target.value)}>
