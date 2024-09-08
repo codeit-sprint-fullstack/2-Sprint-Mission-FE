76553b27add2b2f8271153f32bc5e6103713b141
TaeyeonKim <gggm0117@naver.com>
2024-09-08 21:12:37 +0900

    Implement pageSize as state variable to adjust with window resize.

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,42 @@ import Footer from "./Footer/Footer.js";
 import { getProductList } from "../api/ProductService.js";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import test from '../api/test.js';
+import test from "../api/test.js";
 
 function App() {
   const [bestList, setBestList] = useState([]);
   const [sellingList, setSellingList] = useState([]);
   const [order, setOrder] = useState("recent");
-  const [keyword, setKeyword] = useState('');
+  const [keyword, setKeyword] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPage, setTotalPage] = useState(0);
+  const getPageSize = (width) => {
+    if (width > 1200)
+      return {
+        bestList: 4,
+        sellingList: 10,
+      };
+    if (width > 744)
+      return {
+        bestList: 2,
+        sellingList: 6,
+      };
+    return {
+      bestList: 1,
+      sellingList: 4,
+    };
+  };
+  const [pageSize, setPageSize] = useState(getPageSize(window.innerWidth));
 
+  useEffect(() => {
+    const handleReSize = () => {
+      setPageSize(getPageSize(window.innerWidth));
+    };
+    window.addEventListener("resize", handleReSize);
+    return () => window.removeEventListener("resize", handleReSize);
+  }, []);
+
+  const handleChangePageSize = () => {};
   const handleBestLoad = async (options) => {
     const { list, totalCount } = await test(options);
     setBestList(list);
@@ -22,17 +48,17 @@ function App() {
   const handleSellingLoad = async (options) => {
     const { list, totalCount } = await test(options);
     setSellingList(list);
-    const nextTotalPage = Math.ceil(totalCount / (options.pageSize));
+    const nextTotalPage = Math.ceil(totalCount / options.pageSize);
     setTotalPage(nextTotalPage);
   };
   const handleChangePage = async (pageValue) => {
     setCurrentPage(pageValue);
     const options = {
       page: pageValue,
-      pageSize: 10,
+      pageSize: pageSize.sellingList,
       orderBy: order,
     };
-    console.log('바뀌는 페이지 정보');
+    console.log("바뀌는 페이지 정보");
     console.log(`page: ${options.page}`);
     console.log(`pageSize: ${options.pageSize}`);
     console.log(`orderyBy: ${options.orderBy}`);
@@ -40,12 +66,17 @@ function App() {
   };
   const handleChangeOrder = (chosenOrder) => {
     setOrder(chosenOrder);
-  }
+  };
   const handleChangeKeyword = (inputKeyword) => setKeyword(inputKeyword);
   useEffect(() => {
-    handleBestLoad({ pageSize: 4, orderBy: "favoriteCount" });
-    handleSellingLoad({ page: currentPage, pageSize: 10, orderBy: order, keyword, });
-  }, [order, currentPage, keyword]);
+    handleBestLoad({ pageSize: pageSize.bestList, orderBy: "favoriteCount" });
+    handleSellingLoad({
+      page: currentPage,
+      pageSize: pageSize.sellingList,
+      orderBy: order,
+      keyword,
+    });
+  }, [order, currentPage, keyword, pageSize.bestList]);
 
   const totalList = {
     bestList,
