4f9df5253d289eb6e6bbe800ade54029dd4789df
unknown <d653@naver.com>
2024-09-08 20:19:01 +0900

    Feat: create react-sprint
    sprint-mission5

---
+++ react-sprint/src/components/Header.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import "./Header.css";
+import logoImg from "../imgs/logoImg.svg";
+import mobileLogoImg from "../imgs/mobileLogoImg.svg";
+import userAvatar from "../imgs/userImg.svg";
+
+export default function Header() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const isMobile = windowWidth <= 743;
+  const isTablet = windowWidth >= 744 && windowWidth < 1200;
+
+  return (
+    <header>
+      <a className="logo" href="/" target="_self">
+        <img id="logo" alt="logo" src={isMobile ? mobileLogoImg : logoImg} />
+      </a>
+      <nav className="navbar">
+        <ul className="navbarMenu">
+          <li>
+            <a className="navbarList" href="/자유게시판">
+              자유게시판
+            </a>
+          </li>
+          <li>
+            <a className="navbarList" href="/중고마켓" target="_self">
+              중고마켓
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="user">
+        {isTablet || isMobile ? (
+          <img alt="userAvatar" src={userAvatar} />
+        ) : (
+          <a href="/login" target="_self">
+            <button className="userBtn">로그인</button>
+          </a>
+        )}
+        <p className="userName">{isTablet ? "김코드" : ""}</p>
+      </div>
+    </header>
+  );
+}
---
+++ react-sprint/src/components/ProductList.js
@@ -0,0 +1,148 @@
+import { useState, useEffect } from "react";
+import useOptionProducts from "../hooks/useOptionProducts";
+import Product from "./Product";
+import "./ProductList.css";
+import leftArrowImg from "../imgs/arrowLeft.svg";
+import rightArrowImg from "../imgs/arrowRight.svg";
+import dropdownArrowImg from "../imgs/arrowDown.svg";
+import dropdownSortImg from "../imgs/sortImg.svg";
+import searchImg from "../imgs/searchImg.svg";
+
+function getPageSize() {
+  const width = window.innerWidth;
+
+  if (width >= 1200) {
+    return 10;
+  } else if (width >= 744) {
+    return 6;
+  } else {
+    return 4;
+  }
+}
+
+export default function ProductList() {
+  const [page, setPage] = useState(1);
+  const [order, setOrder] = useState("createdAt");
+  const [pageSize, setPageSize] = useState(getPageSize);
+  const { items, totalCount } = useOptionProducts(page, pageSize, order);
+  const [dropdown, setDropdown] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 743);
+
+  const sortedItems = items.sort((a, b) => {
+    if (!a[order] || !b[order]) return 0;
+    return b[order] - a[order];
+  });
+
+  const totalPages = Math.ceil(totalCount / pageSize);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPageSize(getPageSize());
+      setIsMobile(window.innerWidth <= 743);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    }; // 메모리 누수와 중복 호출 방지
+  }, []);
+
+  function pageNumbers() {
+    const Numbers = [];
+    const maxNumbers = 5;
+    let primaryPage = Math.max(1, page - Math.floor(maxNumbers / 2));
+    let lastPage = Math.min(totalPages, primaryPage + maxNumbers - 1);
+
+    if (lastPage - primaryPage < maxNumbers - 1) {
+      primaryPage = Math.max(1, lastPage - maxNumbers + 1);
+    }
+
+    for (let i = primaryPage; i <= lastPage; i++) {
+      Numbers.push(
+        <button
+          key={i}
+          onClick={() => setPage(i)}
+          className={`paginationBtn ${page === i ? "active" : ""}`}
+        >
+          {i}
+        </button>
+      );
+    }
+    return Numbers;
+  }
+
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
+  const handleNumberClick = (value) => {
+    setOrder(value);
+    setDropdown(false);
+  };
+
+  return (
+    <div className="productList">
+      <div className="productHeader">
+        <h2 className="productHeaderText">판매 중인 상품</h2>
+        <div className="productSearch">
+          <img src={searchImg} alt="Search" />
+          <input
+            type="text"
+            placeholder="검색할 상품을 입력해주세요"
+            className="productSearchInput"
+          />
+        </div>
+        <button className="createBtn">상품 등록하기</button>
+        <div className="Dropdown" onClick={toggleDropdown}>
+          <div className="dropdownSelected">
+            {!isMobile && (order === "createdAt" ? "최신순" : "좋아요순")}
+            <img
+              src={isMobile ? dropdownSortImg : dropdownArrowImg}
+              alt="Dropdown"
+              className="dropdown-arrow"
+            />
+          </div>
+          {dropdown && (
+            <ul className="dropdownList">
+              <li
+                className="itemCreatedAt"
+                onClick={() => handleNumberClick("createdAt")}
+              >
+                최신순
+              </li>
+              <li
+                className="itemFavorite"
+                onClick={() => handleNumberClick("favoriteCount")}
+              >
+                좋아요순
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+      <div className="productGrid">
+        {sortedItems.map((item) => (
+          <Product key={item.id} item={item} />
+        ))}
+      </div>
+      <div className="pagination">
+        <button
+          className="paginationBtn"
+          onClick={() => setPage(page - 1)}
+          disabled={page === 1}
+        >
+          <img src={leftArrowImg} alt="Left" />
+        </button>
+        {pageNumbers()}
+        <button
+          className="paginationBtn"
+          onClick={() => setPage(page + 1)}
+          disabled={page === totalPages}
+        >
+          <img src={rightArrowImg} alt="Right" />
+        </button>
+      </div>
+    </div>
+  );
+}
