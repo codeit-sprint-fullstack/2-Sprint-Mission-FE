5b9dc692d4d1391e8f9b7fc9018ea3803d850241
kipid <kipacti@gmail.com>
2024-09-10 13:54:52 +0900

    sprint5-dist\

--- panda-market/react-app/src/index.js
+++ panda-market/react-app/src/index.js
@@ -6,6 +6,7 @@ import './reset.css';
 import CommonsPage from './pages/CommonsPage.js';
 import HomePage from './pages/HomePage.js';
 import ItemsPage from './pages/ItemsPage.js';
+import RegisPage from './pages/RegisPage.js';
 import LogInPage from './pages/LogInPage.js';
 import SignUpPage from './pages/SignUpPage.js';
 
@@ -16,9 +17,10 @@ root.render(
 			<Route path="/" element={<CommonsPage/>}>
 				<Route index element={<HomePage/>}/>
 				<Route path="items" element={<ItemsPage/>}/>
+				<Route path="registration" element={<RegisPage/>}/>
+				<Route path="login" element={<LogInPage/>}/>
+				<Route path="signup" element={<SignUpPage/>}/>
 			</Route>
-			<Route path="/login" element={<LogInPage/>}/>
-			<Route path="/signup" element={<SignUpPage/>}/>
 			<Route path="*" element={<NotFoundPage/>}/>
 		</Routes>
 	</BrowserRouter>
