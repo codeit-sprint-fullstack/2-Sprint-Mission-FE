c8f060864d9c9a4c6a89c7195d9a22a1e44e84b4
bangsinchur <sinchurbang123@gmail.com>
2024-09-11 17:32:33 +0900

    gitignore Update2

--- scripts/constants.js
+++
@@ -1,8 +0,0 @@
-const USER_DATA = [
-  { email: "codeit1@codeit.com", password: "codeit101!" },
-  { email: "codeit2@codeit.com", password: "codeit202!" },
-  { email: "codeit3@codeit.com", password: "codeit303!" },
-  { email: "codeit4@codeit.com", password: "codeit404!" },
-  { email: "codeit5@codeit.com", password: "codeit505!" },
-  { email: "codeit6@codeit.com", password: "codeit606!" },
-];
