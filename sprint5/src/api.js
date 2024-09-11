557ac11b6d9726af1ed3a6106698cfb9ec6eaa24
Seo-Yurim <yurim2204@naver.com>
2024-09-11 09:27:52 +0900

    Refactor(mentor): .vscode 제거

--- sprint5/src/api.js
+++ sprint5/src/api.js
@@ -1,16 +1,3 @@
-// export async function getProduct({
-//   page = 1,
-//   pageSize = 10,
-//   order = 'createdAt',
-//   keyword = ''
-// }) {
-//   const query = `page=${page}&pageSize=${pageSize}&order=${order}&keyword=${keyword}`;
-//   const res = await fetch(
-//     `https://panda-market-api.vercel.app/products?${query}`
-//   );
-//   const data = await res.json();
-//   return data;
-// }
 import axios from 'axios';
 
 const instance = axios.create({
