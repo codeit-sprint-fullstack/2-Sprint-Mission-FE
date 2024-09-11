241b86cd615dba06a1f062534a7d2d73b0ef30e4
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-09-11 17:37:59 +0900

    [fix] Edit landing page section css [Design] Add a little CSS to the registration page

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import Header from "../Default/Header";
-import Footer from "../Default/Footer";
+import "./Registration.css";
 
 const Registration = () => {
   const [productName, setProductName] = useState("");
@@ -14,47 +13,51 @@ const Registration = () => {
   };
   return (
     <>
-      <h2 className="registration">상품 등록하기</h2>
-      <button className="product-register">등록</button>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>상품명</label>
-          <input
-            type="text"
-            placeholder="상품명을 입력해주세요"
-            onChange={(e) => setProductName(e.target.value)}
-            value={productName}
-          ></input>
+      <main>
+        <div className="registration-container">
+          <h1 className="registration">상품 등록하기</h1>
+          <button className="register-button">등록</button>
         </div>
-        <div>
-          <label>상품 소개</label>
-          <input
-            type="textarea"
-            placeholder="상품 소개를 입력해주세요"
-            value={productDescription}
-            onChange={(e) => setProductDescription(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label>판매가격</label>
-          <input
-            type="number"
-            placeholder="판매 가격을 입력해주세요"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label>태그</label>
-          <input
-            type="text"
-            placeholder="태그를 입력해주세요"
-            value={tags}
-            onChange={(e) => setTags(e.target.value)}
-          ></input>
-        </div>
-        {/* 태그를 엔터쳤을 때 아래에 #추가와 text를 띄워야함 */}
-      </form>
+        <form onSubmit={handleSubmit} className="product-contents-container">
+          <div className="product-wrapper">
+            <label>상품명</label>
+            <input
+              type="text"
+              placeholder="상품명을 입력해주세요"
+              onChange={(e) => setProductName(e.target.value)}
+              value={productName}
+            ></input>
+          </div>
+          <div className="product-wrapper">
+            <label>상품 소개</label>
+            <input
+              type="textarea"
+              placeholder="상품 소개를 입력해주세요"
+              value={productDescription}
+              onChange={(e) => setProductDescription(e.target.value)}
+            ></input>
+          </div>
+          <div className="product-wrapper">
+            <label>판매가격</label>
+            <input
+              type="number"
+              placeholder="판매 가격을 입력해주세요"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            ></input>
+          </div>
+          <div className="product-wrapper">
+            <label>태그</label>
+            <input
+              type="text"
+              placeholder="태그를 입력해주세요"
+              value={tags}
+              onChange={(e) => setTags(e.target.value)}
+            ></input>
+          </div>
+          {/* 태그를 엔터쳤을 때 아래에 #추가와 text를 띄워야함 */}
+        </form>
+      </main>
     </>
   );
 };
