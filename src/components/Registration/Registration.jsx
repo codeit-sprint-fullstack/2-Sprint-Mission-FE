d12e276cdaf9504c68bada5e87c27031cf4c492f
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-09-12 10:48:21 +0900

    [fix] Update Registration page CSS

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -13,51 +13,50 @@ const Registration = () => {
   };
   return (
     <>
-      <main>
-        <div className="registration-container">
-          <h1 className="registration">상품 등록하기</h1>
-          <button className="register-button">등록</button>
+      <div className="registration-container">
+        <h1 className="registration">상품 등록하기</h1>
+        <button className="register-button">등록</button>
+      </div>
+      <form onSubmit={handleSubmit} className="product-contents-container">
+        <div className="product-wrapper">
+          <label>상품명</label>
+          <input
+            type="text"
+            placeholder="상품명을 입력해주세요"
+            onChange={(e) => setProductName(e.target.value)}
+            value={productName}
+          ></input>
         </div>
-        <form onSubmit={handleSubmit} className="product-contents-container">
-          <div className="product-wrapper">
-            <label>상품명</label>
-            <input
-              type="text"
-              placeholder="상품명을 입력해주세요"
-              onChange={(e) => setProductName(e.target.value)}
-              value={productName}
-            ></input>
-          </div>
-          <div className="product-wrapper">
-            <label>상품 소개</label>
-            <input
-              type="textarea"
-              placeholder="상품 소개를 입력해주세요"
-              value={productDescription}
-              onChange={(e) => setProductDescription(e.target.value)}
-            ></input>
-          </div>
-          <div className="product-wrapper">
-            <label>판매가격</label>
-            <input
-              type="number"
-              placeholder="판매 가격을 입력해주세요"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            ></input>
-          </div>
-          <div className="product-wrapper">
-            <label>태그</label>
-            <input
-              type="text"
-              placeholder="태그를 입력해주세요"
-              value={tags}
-              onChange={(e) => setTags(e.target.value)}
-            ></input>
-          </div>
-          {/* 태그를 엔터쳤을 때 아래에 #추가와 text를 띄워야함 */}
-        </form>
-      </main>
+        <div className="product-wrapper">
+          <label>상품 소개</label>
+          <textarea
+            placeholder="상품 소개를 입력해주세요"
+            value={productDescription}
+            onChange={(e) => setProductDescription(e.target.value)}
+          />
+        </div>
+        <div className="product-wrapper">
+          <label>판매가격</label>
+          <input
+            type="number"
+            placeholder="판매 가격을 입력해주세요"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          ></input>
+        </div>
+        <div className="product-wrapper">
+          <label>태그</label>
+          <input
+            type="text"
+            placeholder="태그를 입력해주세요"
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+          ></input>
+        </div>
+        <div className="tags-hardcoding">#티셔츠</div>
+        <div className="tags-hardcoding">#상의</div>
+        {/* 태그를 엔터쳤을 때 아래에 #추가와 text를 띄워야함 */}
+      </form>
     </>
   );
 };
