b4c8b15f64d86ca7c25db372a1f473c9e3dc3c5d
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-09-12 17:45:26 +0900

    [fix] Header CSS update

--- src/components/Default/Header.jsx
+++ src/components/Default/Header.jsx
@@ -1,33 +1,28 @@
 import React from "react";
 import logoImg from "../../images/icon/pandalogo.svg";
-import userIcon from "../../images/etc/userIcon.svg";
+
 import "./HeaderFooter.css";
 
 function Header() {
+  // const handleMarketClick = () => {
+  // navigation("/items");
+  // };
   return (
-    <>
-      <header>
+    <header className="header">
+      <div className="nav-wrapper">
+        <img src={logoImg} alt="판다마켓 로고" className="header-logo" />
         <nav>
-          <div className="logo">
-            <a href="/">
-              <img src={logoImg} alt="판다마켓 로고" />
-            </a>
-          </div>
-          <div className="board">
-            <span id="free-board">
-              <a href="">자유게시판</a>
-            </span>
-            <span id="used-market">
-              <a href="">중고마켓 </a>
-            </span>
-          </div>
+          <ul className="board">
+            <li id="free-board">자유게시판</li>
+            <li id="used-market">중고마켓</li>
+          </ul>
         </nav>
-        <div className="user">
-          <img src={userIcon} alt="유저 이미지" />
-          김코드
-        </div>
-      </header>
-    </>
+      </div>
+
+      <button type="click" className="loginLink button">
+        로그인
+      </button>
+    </header>
   );
 }
 
