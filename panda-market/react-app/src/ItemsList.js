6c4d70710e54a5b9d2cb0a4a58c2517c60492684
kipid <kipacti@gmail.com>
2024-09-13 20:52:17 +0900

    Revert "flex to grid"
    This reverts commit 8c6882c9277b475fa07285ab304e6a1e2538275a.

--- panda-market/react-app/src/ItemsList.js
+++ panda-market/react-app/src/ItemsList.js
@@ -6,9 +6,8 @@ function ItemsList({ items, orderBy, setOrderBy, keyword, setKeyword, onSearch }
 	return (
 		<section className={[styles.section, styles.sec_items].join(" ")}>
 			<div className={styles.items_head}>
-				<h2 style={{ gridArea: "h2" }}>판매 중인 상품</h2>
-				<div style={{ gridArea: "blank0" }}></div>
-				<div style={{ gridArea: "keyword" }} className={styles.items_head_query}>
+				{window.innerWidth > 744 ? <><h2>판매 중인 상품</h2>
+				<div className={styles.items_head_query}>
 					<div className={styles.input_wrapper}>
 						<input type="text" value={keyword} onKeyDown={(e) => {
 							if (e.code === "Enter") {
@@ -18,13 +17,26 @@ function ItemsList({ items, orderBy, setOrderBy, keyword, setKeyword, onSearch }
 						}} onChange={(e) => setKeyword(e.target.value)}/>
 						<img src="/images/ic_search.svg" alt="Search" onClick={onSearch}/>
 					</div>
-					<Link style={{ gridArea: "regis" }} to="/registration"><div className={styles.post_product}>상품 등록하기</div></Link>
-					<div style={{ gridArea: "blank1" }}></div>
-					<select style={{ gridArea: "order" }} className={styles.select_order_by} value={orderBy} onChange={(e) => setOrderBy(e.target.value)}>
+					<Link to="/registration"><div className={styles.post_product}>상품 등록하기</div></Link>
+					<select className={styles.select_order_by} value={orderBy} onChange={(e) => setOrderBy(e.target.value)}>
 						<option value="recent">최신순</option>
 						<option value="favorite">좋아요순</option>
 					</select>
+				</div></>
+				: <><div className={styles.head_row}>
+					<h2>판매 중인 상품</h2>
+					<Link to="/registration"><div className={styles.post_product}>상품 등록하기</div></Link>
 				</div>
+				<div className={styles.head_row}>
+					<div className={styles.input_wrapper}>
+						<input type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
+						<img src="/images/ic_search.svg" alt="Search"/>
+					</div>
+					<select className={styles.select_order_by} value={orderBy} onChange={(e) => setOrderBy(e.target.value)}>
+						<option value="recent">최신순</option>
+						<option value="favorite">좋아요순</option>
+					</select>
+				</div></>}
 			</div>
 			<ul className={[styles.items, styles.normal].join(" ")}>
 				{items.map(item => <Item key={item.id} item={item}/>)}
