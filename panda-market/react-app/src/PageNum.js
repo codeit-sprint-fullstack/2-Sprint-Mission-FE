e026af2098f9137614e9aadac6ce587abfdc50ae
kipid <kipacti@gmail.com>
2024-09-13 22:41:22 +0900

    Update PageNum.js

--- panda-market/react-app/src/PageNum.js
+++ panda-market/react-app/src/PageNum.js
@@ -22,19 +22,21 @@ function PageNum({pageNum, setPageNum, pageNumMax}) {
 				setPageNum(targetN);
 			}
 	}}>
-		<div className={pageNum <= 3 ? styles.disabled : ""}>&lt;</div>{(pageNum <= 1) ?
-		<><div className={styles.selected}>1</div>{pageNumMax >= 2 && <div>2</div>}{pageNumMax >= 3 && <div>3</div>}{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}<div className={pageNumMax <= 5 ? styles.disabled : ""}>&gt;</div></>
+		<div>&lt;</div>
+		{(pageNum <= 1) ?
+		<><div className={styles.selected}>1</div>{pageNumMax >= 2 && <div>2</div>}{pageNumMax >= 3 && <div>3</div>}{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}</>
 		:(pageNum <= pageNumMax && pageNum === 2) ?
-		<><div>1</div><div className={styles.selected}>2</div>{pageNumMax >= 3 && <div>3</div>}{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}<div className={pageNumMax <= 5 ? styles.disabled : ""}>&gt;</div></>
+		<><div>1</div><div className={styles.selected}>2</div>{pageNumMax >= 3 && <div>3</div>}{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}</>
 		:(pageNum <= pageNumMax && pageNum === 3) ?
-		<><div>1</div><div>2</div><div className={styles.selected}>3</div>{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}<div className={pageNumMax <= 5 ? styles.disabled : ""}>&gt;</div></>
+		<><div>1</div><div>2</div><div className={styles.selected}>3</div>{pageNumMax >= 4 && <div>4</div>}{pageNumMax >= 5 && <div>5</div>}</>
 		:(pageNum < pageNumMax - 2 && pageNum > 3) ?
-		<><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className={styles.selected}>{pageNum}</div><div>{pageNum + 1}</div><div>{pageNum + 2}</div><div>&gt;</div></>
+		<><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className={styles.selected}>{pageNum}</div><div>{pageNum + 1}</div><div>{pageNum + 2}</div></>
 		:(pageNum === pageNumMax - 2) ?
-		<><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className={styles.selected}>{pageNum}</div><div>{pageNum + 1}</div><div>{pageNum + 2}</div><div className={styles.disabled}>&gt;</div></>
+		<><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className={styles.selected}>{pageNum}</div><div>{pageNum + 1}</div><div>{pageNum + 2}</div></>
 		:(pageNum === pageNumMax - 1) ?
-		<><div>{pageNum - 3}</div><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className={styles.selected}>{pageNum}</div><div>{pageNum + 1}</div><div className={styles.disabled}>&gt;</div></>
-		:<><div>{pageNumMax - 4}</div><div>{pageNumMax - 3}</div><div>{pageNumMax - 2}</div><div>{pageNumMax - 1}</div><div className={styles.selected}>{pageNumMax}</div><div className={styles.disabled}>&gt;</div></>}
+		<><div>{pageNum - 3}</div><div>{pageNum - 2}</div><div>{pageNum - 1}</div><div className={styles.selected}>{pageNum}</div><div>{pageNum + 1}</div></>
+		:<><div>{pageNumMax - 4}</div><div>{pageNumMax - 3}</div><div>{pageNumMax - 2}</div><div>{pageNumMax - 1}</div><div className={styles.selected}>{pageNumMax}</div></>}
+		<div>&gt;</div>
 	</div>);
 }
 
