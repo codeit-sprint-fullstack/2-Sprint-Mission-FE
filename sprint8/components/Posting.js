eec0a49ec1b2a2b4038adcb31a0acf3173aa916a
annylee0723 <annylee0723@gmail.com>
2024-10-20 23:36:59 +0900

    style: add Posting css

--- sprint8/components/Posting.js
+++ sprint8/components/Posting.js
@@ -4,11 +4,11 @@ import style from '@/styles/Posting.module.css';
 
 export default function Posting() {
   return (
-    <>
+    <div className={style.container}>
       <Header> 게시글 </Header>
       <Link href="/free-board/create-post">
         <button className={style.button}>글쓰기</button>
       </Link>
-    </>
+    </div>
   );
 }
