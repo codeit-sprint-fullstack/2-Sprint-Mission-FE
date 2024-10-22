ef548f8ae35fb10586f390a5ba2229f956c82805
Seo-Yurim <yurim2204@naver.com>
2024-10-22 11:07:30 +0900

    Feat: Nav, Footer 추가

---
+++ components/Nav/Nav.js
@@ -0,0 +1,50 @@
+import styles from '@/components/Nav/Nav.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Nav() {
+  const router = useRouter();
+
+  const getLinkStyle = (path) => {
+    return {
+      color: router.pathname === path ? '#3692ff' : ''
+    };
+  };
+
+  return (
+    <header className={styles.nav}>
+      <div className={styles.container}>
+        <div className={styles.leftSection}>
+          <Link href="/">
+            <Image
+              className={styles.logo}
+              src="/images/Property 1=sm.png"
+              width={153}
+              height={51}
+              alt="기본 사이트 로고"
+            />
+            <Image
+              className={styles.mobile}
+              src="/images/Property 1=Typo.png"
+              width={81}
+              height={40}
+              alt="모바일 사이트 로고"
+            />
+          </Link>
+          <ul className={styles.menu}>
+            <li>
+              <Link href="/">자유게시판</Link>
+            </li>
+            <li>
+              <Link href="/items">중고마켓</Link>
+            </li>
+          </ul>
+        </div>
+        <button className={styles.auth}>
+          <Link href="/login">로그인</Link>
+        </button>
+      </div>
+    </header>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import Head from 'next/head';
+import Nav from '@/components/Nav/Nav';
+import Footer from '@/components/Footer/Footer';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>판다마켓</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
