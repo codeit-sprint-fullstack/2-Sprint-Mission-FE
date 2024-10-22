aca710930f327267dcd9301eb237fb0c90182f74
annylee0723 <annylee0723@gmail.com>
2024-10-22 09:38:10 +0900

    style: add SearchBar css

---
+++ sprint8/components/SearchBar.js
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import magnifierIcon from '@/public/assets/ic_search.png';
+import style from '@/styles/SearchBar.module.css';
+
+export default function SearchBar() {
+  return (
+    <div className={style.searchBar}>
+      <Image src={magnifierIcon} alt="magifier icon" />
+      <input
+        id="searchPostInput"
+        className={style.searchInput}
+        placeholder="검색할 상퓸을 입력해주세요"
+      />
+    </div>
+  );
+}
--- sprint8/pages/free-board/index.js
+++ sprint8/pages/free-board/index.js
@@ -2,6 +2,7 @@ import Posting from '@/components/Posting.js';
 import BestProduct from '@/components/BestProduct';
 import Header from '@/components/Header.js';
 import style from '@/styles/free-board.module.css';
+import SearchBar from '@/components/SearchBar';
 
 export default function FreeBoard() {
   return (
@@ -9,6 +10,7 @@ export default function FreeBoard() {
       <Header> 베스트 게시글 </Header>
       <BestProduct />
       <Posting />
+      <SearchBar />
     </div>
   );
 }
