9495ae02ba0c3bda9beeafe5c17dc8f75b3e7dc6
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-10-22 17:43:47 +0900

    feat: post register ui update

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,6 +24,11 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/board.js"
+    ],
+    "/board/register": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/board/register.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("36188002a6f7f2a0")
+/******/ 		__webpack_require__.h = () => ("bf926f9a95eebc7a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
