e63bb6b52f63aaa9f0c924b842074361a9c987eb
annylee0723 <annylee0723@gmail.com>
2024-10-22 13:11:54 +0900

    style: add PostList css

--- sprint8/components/PostList.js
+++ sprint8/components/PostList.js
@@ -1,23 +1,26 @@
 import Image from 'next/image';
 import profileImg from '@/public/assets/img_profile.png';
 import heartIcon from '@/public/assets/ic_heart.png';
+import style from '@/styles/PostList.module.css';
 
 export default function PostList() {
   return (
-    <div>
-      <div>
-        <h1>맥북 16인치 16기가 11테라 정도 사양이면 어쩌고</h1>
-        <Image />
+    <div className={style.container}>
+      <div className={style.titleAndImage}>
+        <h1 className={style.title}>
+          맥북 16인치 16기가 11테라 정도 사양이면 어쩌고
+        </h1>
+        <Image className={style.postImage} />
       </div>
-      <div>
-        <div>
-          <Image src={profileImg} alt="a white panda with grey background" />
-          <p>총명한 판다</p>
-          <p>2024. 04. 16</p>
+      <div className={style.bottom}>
+        <div className={style.bottomSection}>
+          <Image src={profileImg} alt="a white panda with grey background" className={style.profileImg}/>
+          <p className={style.nickname}>총명한 판다</p>
+          <p className={style.date}>2024. 04. 16</p>
         </div>
-        <div>
+        <div className={style.bottomSection}>
           <Image src={heartIcon} alt="heart icon" />
-          <p>9999+</p>
+          <p className={style.likes}>9999+</p>
         </div>
       </div>
     </div>
