fa826309721eb09388de9c58e76249ee5d3ccb05
annylee0723 <annylee0723@gmail.com>
2024-10-22 15:43:38 +0900

    refactor: add textarea tag in order to use multi-line inputting

--- sprint8/components/CreatePost.js
+++ sprint8/components/CreatePost.js
@@ -2,13 +2,25 @@ import Header from '@/components/Header.js';
 import style from '@/styles/CreatePost.module.css';
 
 export default function CreatePost({ title, placehold }) {
-  const input = title === '*제목' ? '.postTitle' : '.contnet';
+  const inputType = title === '*제목' ? 'postTitle' : 'contnet';
 
   return (
-    <div>
+    <div className={style.container}>
       <Header>{title}</Header>
-      <div className={style[input]}>
-        <input placeholder={placehold}></input>
+      <div className={style[inputType]}>
+        {inputType === 'postTitle' ? (
+          <input
+            id={inputType}
+            placeholder={placehold}
+            className={style.input}
+          />
+        ) : (
+          <textarea
+            id={inputType}
+            placeholder={placehold}
+            className={style.textarea}
+          />
+        )}
       </div>
     </div>
   );
