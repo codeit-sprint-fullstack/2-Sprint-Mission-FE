38a6fd65a8fd26346bf20b62e72ab34b5d13d612
Seo-Yurim <yurim2204@naver.com>
2024-10-22 18:57:04 +0900

    Feat & Design: 게시판 상세 페이지 헤더 구현

---
+++ pages/articles/[id].js
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import styles from '@/styles/Article.module.css';
+import axios from '@/lib/axios';
+import ArticleCommentList from '@/components/ArticleList/ArticleList';
+import formatDate from '@/lib/formatDate';
+
+export default function article() {
+  const [article, setArticle] = useState();
+  const [articleComments, setArticleComments] = useState([]);
+  const router = useRouter();
+  const id = router.query['id'];
+
+  async function loadArticle(targetId) {
+    const res = await axios.get(`/articles/${targetId}`);
+    const article = res.data;
+    setArticle(article);
+  }
+
+  async function loadArticleCommnets(targetId) {
+    const res = await axios.get(`/articles/${targetId}/comments`);
+    const articleComments = res.data ?? [];
+    setArticleComments(articleComments);
+  }
+
+  useEffect(() => {
+    if (id) {
+      loadArticle(id);
+      loadArticleCommnets(id);
+    }
+  }, [id]);
+
+  if (!article) return null;
+
+  return (
+    <div>
+      <div className={styles.article}>
+        <div className={styles[`article-header`]}>
+          <div className={styles[`article-title`]}>
+            <h1>{article.title}</h1>
+            <Image
+              src="/images/ic_kebab.png"
+              width={24}
+              height={24}
+              alt="메뉴 아이콘"
+            />
+          </div>
+          <div className={styles.info}>
+            <div className={styles[`user-info`]}>
+              <Image
+                src="/images/size=large.png"
+                width={40}
+                height={40}
+                alt="유저 아이콘"
+              />
+              <p>판매왕 판다</p>
+              <p>{formatDate(article.createdAt)}</p>
+            </div>
+            <div className={styles[`like-wrap`]}>
+              <div className={styles.like}>
+                <Image
+                  src="/images/ic_heart.png"
+                  width={32}
+                  height={32}
+                  alt="좋아요 아이콘"
+                />
+                <p>+9999</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p>{article.content}</p>
+      </div>
+    </div>
+  );
+}
