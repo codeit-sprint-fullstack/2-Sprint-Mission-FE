547bb2513d377b25e2d358047e15c4403c77f662
annylee0723 <annylee0723@gmail.com>
2024-10-22 18:04:32 +0900

    feat: add AddComment component

---
+++ sprint8/components/AddComment.js
@@ -0,0 +1,13 @@
+import Button from '@/components/Button.js';
+
+export default function AddComment() {
+  return (
+    <div>
+      <a>댓글 달기</a>
+      <div>
+        <textarea />
+      </div>
+      <Button>작성</Button>
+    </div>
+  );
+}
--- sprint8/components/PostDetailInfo.js
+++ sprint8/components/PostDetailInfo.js
@@ -1,27 +1,35 @@
+import Image from 'next/image';
+import style from '@/styles/PostDetailInfo.module.css';
+import profileImg from '@/public/assets/img_profile.png';
+import heartIcon from '@/public/assets/ic_heart.png';
+
 export default function PostDetail() {
   return (
-    <div>
+    <>
       <div className={style.container}>
-        <div className={style.titleAndImage}>
+        <div className={style.titleAndKebab}>
           <h1 className={style.title}>
             맥북 16인치 16기가 11테라 정도 사양이면 어쩌고
           </h1>
-          <Image className={style.postImage} />
         </div>
         <div className={style.bottom}>
-          <div className={style.bottomSection}>
+          <div className={style.profileContainer}>
             <Image src={profileImg} alt="a white panda with grey background" />
-            <div>
+            <div className={style.nicknameDataContainer}>
               <p className={style.nickname}>총명한 판다</p>
               <p className={style.date}>2024. 04. 16</p>
             </div>
           </div>
-          <div className={style.bottomSection}>
+          <hr className={style.line} />
+          <div className={style.likeContainer}>
             <Image src={heartIcon} alt="heart icon" />
-            <p className={style.likes}>9999+</p>
+            <p className={style.likes}>123</p>
           </div>
         </div>
       </div>
-    </div>
+      <div className={style.content}>
+        맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?
+      </div>
+    </>
   );
 }
