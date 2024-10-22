6e91aa566b2832338eb288cf5145a9c20fe9ce9a
unknown <d653@naver.com>
2024-10-22 21:25:53 +0900

    Style: Nav, Footer

--- next-sprint/components/Nav.js
+++ next-sprint/components/Nav.js
@@ -19,21 +19,23 @@ export default function Nav() {
   }, []);
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.navPanda}>
-        {windowWidth > 743 ? (
-          <Image fill src="/images/nav_panda.svg" alt="navPanda" />
-        ) : (
-          <Image fill src="/images/nav_panda_mobile.svg" alt="navPanda" />
-        )}
-      </div>
-      <div className={styles.menu}>
-        <h3>자유게시판</h3>
-        <h3>중고마켓</h3>
-      </div>
-      <button type="submit" className={styles.login}>
-        로그인
-      </button>
-    </nav>
+    <div className={styles.navContainer}>
+      <nav className={styles.nav}>
+        <div className={styles.navPanda}>
+          {windowWidth > 743 ? (
+            <Image fill src="/images/nav_panda.svg" alt="navPanda" />
+          ) : (
+            <Image fill src="/images/nav_panda_mobile.svg" alt="navPanda" />
+          )}
+        </div>
+        <div className={styles.menu}>
+          <h3>자유게시판</h3>
+          <h3>중고마켓</h3>
+        </div>
+        <button type="submit" className={styles.login}>
+          로그인
+        </button>
+      </nav>
+    </div>
   );
 }
