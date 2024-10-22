09719c47882a35304ad5d4114d40aba959598a35
annylee0723 <annylee0723@gmail.com>
2024-10-22 18:20:43 +0900

    style: add AddComment css

--- sprint8/components/AddComment.js
+++ sprint8/components/AddComment.js
@@ -1,13 +1,18 @@
+import style from '@/styles/AddComment.module.css';
 import Button from '@/components/Button.js';
 
 export default function AddComment() {
   return (
-    <div>
-      <a>댓글 달기</a>
-      <div>
-        <textarea />
+    <div className={style.container}>
+      <a className={style.title}>댓글 달기</a>
+      <div className={style.comment}>
+        <textarea
+          id="comment"
+          placeholder="코멘트를 입력해주세요"
+          className={style.textarea}
+        />
       </div>
-      <Button>작성</Button>
+      <Button className={style.button}>작성</Button>
     </div>
   );
 }
