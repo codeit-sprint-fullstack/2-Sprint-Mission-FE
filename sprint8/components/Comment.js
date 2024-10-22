389cbf55fc83db250b0f6c0d22577ab22e41a1f6
annylee0723 <annylee0723@gmail.com>
2024-10-22 18:58:10 +0900

    style: add Comment css

--- sprint8/components/Comment.js
+++ sprint8/components/Comment.js
@@ -1,17 +1,18 @@
 import Image from 'next/image';
+import style from '@/styles/Comment.module.css';
 import profileImg from '@/public/assets/img_profile.png';
 
 export default function Comment() {
   return (
-    <div>
+    <div className={style.container}>
       <div>
-        <h1>혹시 사용기간이 어떻게 되실까요?</h1>
+        <h1 className={style.title}>혹시 사용기간이 어떻게 되실까요?</h1>
       </div>
-      <div>
+      <div className={style.profile}>
         <Image src={profileImg} alt="a white panda with grey background" />
-        <div>
-          <p>똑똑한 판다</p>
-          <p>1시간 전</p>
+        <div className={style.texts}>
+          <p className={style.nickname}>똑똑한 판다</p>
+          <p className={style.date}>1시간 전</p>
         </div>
       </div>
     </div>
