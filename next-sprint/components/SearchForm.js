ca5fe1d83b95d4c9745708cd0a97da8637e54ed7
unknown <d653@naver.com>
2024-10-22 19:52:30 +0900

    Feat: search

---
+++ next-sprint/components/SearchForm.js
@@ -0,0 +1,32 @@
+import styles from './SearchForm.module.css';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function SearchForm({ initialValue, onChange }) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    const newValue = e.target.value;
+    setValue(newValue);
+    onChange(newValue);
+  }
+
+  return (
+    <form className={styles.form}>
+      <label className={styles.label}>
+        <input
+          placeholder="검색할 상품을 입력해주세요."
+          className={styles.search}
+          onChange={handleChange}
+          value={value}
+        ></input>
+        {!value && (
+          <div className={styles.searchImg}>
+            <Image fill src="/images/ic_search.svg" alt="검색 이미지" />
+          </div>
+        )}
+      </label>
+      <div></div>
+    </form>
+  );
+}
--- next-sprint/pages/index.js
+++ next-sprint/pages/index.js
@@ -2,11 +2,13 @@ import styles from '@/styles/Home.module.css';
 import RecentArticle from '@/components/RecentArticle';
 import ArticleList from '@/components/ArticleList';
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
+import SearchForm from '@/components/SearchForm';
 
 export default function Home() {
   const [recentArticles, setRecentArticles] = useState([]);
   const [articles, setArticles] = useState([]);
+  const [searchArticle, setSearchArticle] = useState([]);
+  const [value, setValue] = useState('');
 
   async function getRecentArticles() {
     const res = await fetch(
@@ -22,10 +24,25 @@ export default function Home() {
     setArticles(articles);
   }
 
+  async function getSearchArticle(value) {
+    if (value) {
+      const res = await fetch(`http://localhost:5000/articles?search=${value}`);
+      const articles = await res.json();
+      setSearchArticle(articles);
+    } else {
+      setSearchArticle([]);
+    }
+  }
+
   useEffect(() => {
     getRecentArticles();
-    getArticleList();
-  }, []);
+
+    if (value) {
+      getSearchArticle(value);
+    } else {
+      getArticleList();
+    }
+  }, [value]);
 
   return (
     <>
@@ -39,19 +56,12 @@ export default function Home() {
             <h2>게시글</h2>
             <button>글쓰기</button>
           </div>
-          <form className={styles.form}>
-            <label className={styles.label}>
-              <input
-                placeholder="검색할 상품을 입력해주세요."
-                className={styles.search}
-              ></input>
-              <div className={styles.searchImg}>
-                <Image fill src="/images/ic_search.svg" alt="검색 이미지" />
-              </div>
-            </label>
-            <div></div>
-          </form>
-          <ArticleList articles={articles} />
+          <SearchForm initialValue={value} onChange={setValue} />
+          {value ? (
+            <ArticleList articles={searchArticle} />
+          ) : (
+            <ArticleList articles={articles} />
+          )}
         </section>
       </div>
     </>
