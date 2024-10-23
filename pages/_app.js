e9205ff6ca53dee79e9f6c5a988a84acbab331d5
Jake <buffso@naver.com>
2024-10-23 11:27:45 +0900

    chore: remove unused files and add global.css setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import "@/styles/variables.css";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <Head>
+      <title>판다마켓</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
+        <link 
+          rel="stylesheet" 
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" 
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
