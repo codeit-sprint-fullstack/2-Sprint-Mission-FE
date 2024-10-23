883e3a0dbc2a86150a628af30a2d7cb4e8731321
Seo-Yurim <yurim2204@naver.com>
2024-10-23 11:31:40 +0900

    Remove: 게시글 등록 컴포넌트 파일 삭제 후 register에 병합

--- pages/articles/register.js
+++ pages/articles/register.js
@@ -1,10 +1,22 @@
-import RegisterForm from '@/components/ArticleRegister/RegisterForm';
 import styles from '@/styles/ArticleRegister.module.css';
 
 export default function Register() {
   return (
     <div>
-      <RegisterForm />
+      <form className={styles.form}>
+        <div className={styles[`form-header`]}>
+          <h1>게시글 쓰기</h1>
+          <button>등록</button>
+        </div>
+        <div className={styles.group}>
+          <label htmlFor="title">* 제목</label>
+          <input type="text" id="title" placeholder="제목을 입력해주세요" />
+        </div>
+        <div className={styles.group}>
+          <label htmlFor="content">* 내용</label>
+          <textarea type="text" id="content" placeholder="내용 입력해주세요" />
+        </div>
+      </form>
     </div>
   );
 }
