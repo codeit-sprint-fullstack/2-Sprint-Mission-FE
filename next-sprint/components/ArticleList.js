8edcad2a9326d52c59bc20ba317fcdee56123c5f
unknown <d653@naver.com>
2024-10-23 13:32:27 +0900

    Feat: 게시글 상세 페이지로 이동 기능

--- next-sprint/components/ArticleList.js
+++ next-sprint/components/ArticleList.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './ArticleList.module.css';
+import Link from 'next/link';
 
 export default function ArticleList({ articles }) {
   return (
@@ -17,28 +18,34 @@ export default function ArticleList({ articles }) {
 
         return (
           <li key={article.id} className={styles.article}>
-            <div className={styles.title}>
-              <h3>{article.title}</h3>
-              <div className={styles.sample}>
-                <Image
-                  width={48}
-                  height={44.57}
-                  src="/images/sample_img.svg"
-                  alt="샘플 이미지"
-                />
+            <Link className={styles.link} href={`/articles/${article.id}`}>
+              <div className={styles.title}>
+                <h3>{article.title}</h3>
+                <div className={styles.sample}>
+                  <Image
+                    width={48}
+                    height={44.57}
+                    src="/images/sample_img.svg"
+                    alt="샘플 이미지"
+                  />
+                </div>
               </div>
-            </div>
-            <div className={styles.user}>
-              <div className={styles.profile}>
-                <Image fill src="/images/ic_profile.svg" alt="프로필 이미지" />
+              <div className={styles.user}>
+                <div className={styles.profile}>
+                  <Image
+                    fill
+                    src="/images/ic_profile.svg"
+                    alt="프로필 이미지"
+                  />
+                </div>
+                <h3>총명한 판다</h3>
+                <h3>{formattedDate}</h3>
+                <div className={styles.heart}>
+                  <Image fill src="/images/ic_heart.svg" alt="좋아요 이미지" />
+                </div>
+                <h3>9999+</h3>
               </div>
-              <h3>총명한 판다</h3>
-              <h3>{formattedDate}</h3>
-              <div className={styles.heart}>
-                <Image fill src="/images/ic_heart.svg" alt="좋아요 이미지" />
-              </div>
-              <h3>9999+</h3>
-            </div>
+            </Link>
           </li>
         );
       })}
