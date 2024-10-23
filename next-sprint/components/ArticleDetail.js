f7cd02c6e0bee5eeb0896e725c9305a1cc5a4f56
unknown <d653@naver.com>
2024-10-23 15:08:01 +0900

    Fix: 게시글 상세 페이지 UI

--- next-sprint/components/ArticleDetail.js
+++ next-sprint/components/ArticleDetail.js
@@ -13,33 +13,37 @@ export default function ArticleDetail({ article }) {
     .slice(0, -1);
 
   return (
-    <ul>
-      <li key={article.id}>
-        <header>
-          <div>
+    <ul className={styles.ul}>
+      <li key={article.id} className={styles.li}>
+        <header className={styles.header}>
+          <div className={styles.title}>
             <h3>{article.title}</h3>
           </div>
-          <div>
-            <div>
+          <div className={styles.user}>
+            <div className={styles.userLeft}>
               <div className={styles.profile}>
                 <Image fill src="/images/ic_profile.svg" alt="프로필 이미지" />
               </div>
               <h3>총명한 판다</h3>
               <h3>{formattedDate}</h3>
             </div>
-            <div>
-              <div className={styles.heart}>
-                <Image fill src="/images/ic_heart.svg" alt="좋아요 이미지" />
-              </div>
+            <bar className={styles.bar}></bar>
+            <div className={styles.userRight}>
+              <Image
+                width={32}
+                height={32}
+                src="/images/ic_heart.svg"
+                alt="좋아요 이미지"
+              />
               <h3>9999+</h3>
             </div>
           </div>
         </header>
-        <main>
-          <h4>{article.content}</h4>
-          <form>
+        <main className={styles.main}>
+          <h4 className={styles.content}>{article.content}</h4>
+          <form className={styles.form}>
             <h4>댓글 달기</h4>
-            <input placeholder="댓글을 입력하세요" />
+            <textarea placeholder="댓글을 입력해주세요."></textarea>
             <button type="submit">등록</button>
           </form>
         </main>
