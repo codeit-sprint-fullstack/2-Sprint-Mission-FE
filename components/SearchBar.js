e33dfb678cfcaac94f7413aa6ca916b8cab3f8b0
alscksdlek <mseon916@naver.com>
2024-10-23 17:25:24 +0900

    style: SearchBar css

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,8 +1,28 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/searchBar.module.css";
 
 export default function SearchBar() {
+  const [smallSize, setSmallSize] = useState(false);
+
+  const handleResize = () => {
+    const windowWidth = window.innerWidth;
+    if (windowWidth <= 743 && windowWidth >= 375) {
+      setSmallSize(true);
+    } else {
+      setSmallSize(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <div className={styles.searchBar}>
@@ -20,14 +40,27 @@ export default function SearchBar() {
         />
         <div className={styles.dropdownContainer}>
           <button className={styles.dropdownButton}>
-            <div>최신순</div>
-            <Image
-              src="/toggleArrow.png"
-              alt="toggleArrow"
-              className={styles.toggleArrow}
-              width={24}
-              height={24}
-            />
+            {!smallSize && (
+              <>
+                <div>최신순</div>
+                <Image
+                  src="/toggleArrow.png"
+                  alt="toggleArrow"
+                  className={styles.toggleArrow}
+                  width={24}
+                  height={24}
+                />
+              </>
+            )}
+            {smallSize && (
+              <Image
+                src="/smallArrow.png"
+                alt="smallArrow"
+                className={styles.smallArrow}
+                width={24}
+                height={24}
+              />
+            )}
           </button>
         </div>
       </div>
