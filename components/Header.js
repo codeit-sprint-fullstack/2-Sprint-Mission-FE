1a3259d9cc1b4f4ab9ecc4dc7fac4cb2d0bfa575
alscksdlek <mseon916@naver.com>
2024-10-23 10:31:17 +0900

    feat: header 컴포넌트 추가

---
+++ components/Header.js
@@ -0,0 +1,65 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import styles from "../styles/Header.module.css";
+import Image from "next/image";
+
+export default function Header() {
+  const router = useRouter();
+  const { pathName } = router;
+  const [imageSrc, setImageSrc] = useState("/logoImg.png");
+  const [imageWidth, setImageWidth] = useState(153);
+  const [imageHeight, setImageHeight] = useState(51);
+  useEffect(() => {
+    const windowResize = () => {
+      if (window.innerWidth <= 743 && window.innerWidth >= 375) {
+        setImageSrc("/smallLogoImg.png");
+        setImageWidth(81);
+        setImageHeight(27);
+      } else {
+        setImageSrc("/logoImg.png");
+        setImageWidth(153);
+        setImageHeight(51);
+      }
+    };
+    window.addEventListener("resize", windowResize);
+    windowResize();
+  }, []);
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerLeft}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image
+              className={styles.logoImg}
+              src={imageSrc}
+              alt="판다마켓 로고"
+              width={imageWidth}
+              height={imageHeight}
+            />
+          </Link>
+        </div>
+        <div>
+          <Link
+            className={
+              pathName === "/items" ? styles.inCommunity : styles.community
+            }
+            href="/community"
+          >
+            자유게시판
+          </Link>
+          <Link
+            className={pathName === "/items" ? styles.inMarket : styles.market}
+            href="/items"
+          >
+            중고마켓
+          </Link>
+        </div>
+      </div>
+      <div className={styles.login}>
+        <button className={styles.loginButton}>로그인</button>
+      </div>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,21 @@
+import Header from "../components/Header";
+import { Noto_Sans, Noto_Sans_KR } from "next/font/google";
+import "../styles/globals.css";
+
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+});
+
+const notoSansKR = Noto_Sans_KR({
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <main className={`${notoSans.className} ${notoSansKR.className}`}>
+        <Header />
+      </main>
+    </>
+  );
 }
