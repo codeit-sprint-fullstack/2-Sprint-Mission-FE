a6bb07583a4286eb87139f732cd508402fdcedf6
annylee0723 <annylee0723@gmail.com>
2024-10-25 00:47:48 +0900

    refactor: change Best Product Rendering method to use server side rendering

--- sprint8/pages/free-board/index.js
+++ sprint8/pages/free-board/index.js
@@ -9,16 +9,9 @@ import Sorting from '@/components/Sorting.js';
 import PostList from '@/components/PostList.js';
 import axios from '@/lib/axios.js';
 
-export default function FreeBoard() {
+export default function FreeBoard({ initialBestArticles }) {
   const [articles, setArticles] = useState([]);
-  const [bestArticles, setBestArticles] = useState([]);
   const [keyword, setKeyword] = useState('');
-
-  async function fetchBestArticles() {
-    const res = await axios.get(`/articles`);
-    const data = await res.data;
-    setBestArticles(data);
-  }
 
   async function fetchArticles(searchKeyword) {
     const res = await axios.get(`/articles?&search=${searchKeyword}`);
@@ -27,14 +20,13 @@ export default function FreeBoard() {
   }
 
   useEffect(() => {
-    fetchBestArticles();
     fetchArticles(keyword);
   }, [keyword]);
 
   return (
     <div className={style.body}>
       <Header> 베스트 게시글 </Header>
-      <BestPosts data={bestArticles} />
+      <BestPosts data={initialBestArticles} />
       <div className={style.headerAndButton}>
         <Header> 게시글 </Header>
         <Link href="/free-board/create-post">
@@ -48,4 +40,25 @@ export default function FreeBoard() {
       <PostList data={articles} />
     </div>
   );
+}
+
+//Best 게시글은 고정된 값이니까 서버에서 받아올 수 있지 않을까? 싶어서 시도
+export async function getServerSideProps() {
+  try {
+    const res = await axios.get(`/articles`);
+    const data = await res.data;
+
+    return {
+      props: {
+        initialBestArticles: data
+      }
+    };
+  } catch (error) {
+    console.error('Failed to get Best Articles:', error);
+    return {
+      props: {
+        initialBestArticles: []
+      }
+    };
+  }
 }
