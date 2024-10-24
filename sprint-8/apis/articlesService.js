f0aba370039612744477f0ac662af6db24897da9
kipid <kipacti@gmail.com>
2024-10-24 17:08:19 +0900

    Update backend port

--- sprint-8/apis/articlesService.js
+++ sprint-8/apis/articlesService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://localhost:3000`
+  baseURL: `http://localhost:3100`
 });
 
 async function getArticles(params = { skip: 0, take: 10, sort: "recent", keyword: "" }) {
