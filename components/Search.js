995f16075babe39d0a08e27efddf9b6a5a703aae
shinji530 <shinji53059@gmail.com>
2024-10-25 16:09:27 +0900

    Style: 게시판 Search 바 크기 조절

--- components/Search.js
+++ components/Search.js
@@ -3,9 +3,12 @@ import style from "./styles/Search.module.css";
 import ic_search from "@/public/ic_search.png";
 import ic_X from "@/public/ic_X.png";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Search({ setSearch }) {
   const [inputValue, setInputValue] = useState("");
+  const router = useRouter();
+  const isPost = router.pathname === "/articles";
 
   const handlesearchChange = (e) => {
     setInputValue(e.target.value);
@@ -26,7 +29,7 @@ export default function Search({ setSearch }) {
   };
 
   return (
-    <div className={style.searchGroup}>
+    <div className={`${style.searchGroup} ${isPost ? style.postSearch : ""}`}>
       <Image className={style.searchIcon} src={ic_search} alt="ic_search" />
       <input
         className={style.search}
