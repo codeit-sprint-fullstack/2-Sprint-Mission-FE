09f2989f66100207e8b2b40dc47629dd7b6bcf5c
Kang Myoung Gon <jin04053@naver.com>
2024-10-26 03:45:00 +0900

    Fix: edit Nav styles and logic

--- conponents/Nav.js
+++ conponents/Nav.js
@@ -1,23 +1,57 @@
 import styles from "./Nav.module.css";
 import Image from "next/image";
 import logo from "@/public/pandamarket_logo.png";
+import mobileLogo from "@/public/mobileLogo.svg";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Nav() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const pathname = usePathname();
+
+  function menuActiveStyle(location) {
+    return pathname && pathname.startsWith(location) ? styles.active : "";
+  }
+
+  useEffect(() => {
+    const handleLogoChang = () => {
+      setIsMobile(window.innerWidth < 744);
+    };
+
+    window.addEventListener("resize", handleLogoChang);
+
+    return () => {
+      window.removeEventListener("resize", handleLogoChang);
+    };
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.logoMenu}>
           <Link href="/">
             <div className={styles.logo}>
-              <Image fill priority src={logo} alt="pandamarket logo" />
+              <Image
+                fill
+                priority
+                src={!isMobile ? logo : mobileLogo}
+                alt="pandamarket logo"
+              />
             </div>
           </Link>
-          <div className={styles.menu}>
-            <Link className={`${styles.menu}`} href="/freeBoard">
+          <div className={styles.menuList}>
+            <Link
+              className={`${styles.menu} ${menuActiveStyle("/freeBoard")}`}
+              href="/freeBoard"
+            >
               자유게시판
             </Link>
-            <Link className={`${styles.menu}`} href="/marketplace">
+            <Link
+              className={`${styles.menu} ${menuActiveStyle("/marketplace")}`}
+              href="/marketplace"
+            >
               중고마켓
             </Link>
           </div>
