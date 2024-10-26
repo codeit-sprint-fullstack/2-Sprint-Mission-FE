51fbf01c5e45a2ec89e05b7a2c8711ebf6515d14
Jake <buffso@naver.com>
2024-10-27 03:54:58 +0900

    add article deletion functionality

--- components/ArticleDetail/ArticleInfo.js
+++ components/ArticleDetail/ArticleInfo.js
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import styles from './ArticleInfo.module.css';
 import Image from 'next/image';
+import { deleteArticle } from '@/lib/api/ArticleService';
+import { useRouter } from 'next/router';
 
 export default function ArticleInfo({ article }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const router = useRouter();
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+  const handleDelete = async () => {
+    const confirmed = window.confirm('삭제하시겠습니까?');
+    if (confirmed) {
+      try {
+        await deleteArticle(article.id);
+
+        router.push('/articles');
+      } catch (error) {
+        alert('게시글 삭제에 실패했습니다.');
+      }
+    } 
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{article.title}</h1>
@@ -36,8 +57,28 @@ export default function ArticleInfo({ article }) {
           </div>
           <span className={styles.likesCount}>{article.likes > 9999 ? '9999+' : article.likes}</span>
         </div>
+
+        {/* 드롭다운 메뉴 */}
+        <div className={styles.dropdownContainer}>
+          <button className={styles.dropdownToggle} onClick={toggleDropdown}>
+            <div className={styles.dropdownIcon}>
+              <Image
+                src="/images/articles/ic_kebab.svg"
+                alt="더보기 메뉴 아이콘"
+                fill
+                sizes='2.4rem'
+              />
+            </div>
+          </button>
+          {dropdownOpen && (
+            <div className={styles.dropdownMenu}>
+              <button className={styles.dropdownItem}>수정하기</button>
+              <button className={styles.dropdownItem} onClick={handleDelete}>삭제하기</button>
+            </div>
+          )}
+        </div>
+
       </div>
-      
       <div className={styles.content}>{article.content}</div>
     </div>
   );
