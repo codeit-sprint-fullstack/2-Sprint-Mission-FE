c89b66c7cb3566623aeb65cbf461685644e4a9c8
annylee0723 <annylee0723@gmail.com>
2024-10-26 22:04:46 +0900

    refactor(mentor): add Prentendard file as local font

--- sprint8/src/pages/_app.js
+++ sprint8/src/pages/_app.js
@@ -3,6 +3,13 @@ import '@/src/styles/reset.css';
 import '@/src/styles/globals.css';
 import '@/src/styles/color.css';
 import Nav from '@/src/components/Nav.js';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920'
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,8 +19,10 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Nav />
-      <Component {...pageProps} />
+      <main className={pretendard.className}>
+        <Nav />
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
