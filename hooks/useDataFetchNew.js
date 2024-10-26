9e2548a3230123065adac41a48ef044cf954df30
TaeyeonKim <gggm0117@naver.com>
2024-10-27 01:09:32 +0900

    feat:apply useDataFetchNew in freeboard/[id].js

---
+++ hooks/useDataFetchNew.js
@@ -0,0 +1,59 @@
+import axios from "@/lib/axios";
+import { useEffect, useState } from "react";
+import useResponsiveItemCount from "./useResponsiveItemCount";
+
+export default function useDataFetch({
+  type,
+  count = 1,
+  order = "recent",
+  page = 0,
+  keyword = "",
+  id = "297f335f-b678-4653-9fe0-186a0e99d037"
+}) {
+  function convertName() {
+    let dataName, url;
+    switch (type) {
+      case "articleBestList":
+        dataName =
+          url = `/articles?keyword=${keyword}&order=${order}&page=${page}&pageSize=${count}`;
+        return { dataName, url };
+      case "articleStandardList":
+        url = `/articles?keyword=${keyword}&order=${order}&page =${page}&pageSize=${count}`;
+        return { model, listName, countName, url };
+      case "articleWithComments":
+        url = `/articles/${id}/withcomments?order=${order}&page=${page}&pageSize=${count}`;
+        return { dataName, url };
+    }
+  }
+  const [data, setData] = useState();
+  const [dataList, setDataList] = useState([]);
+  const [totalCount, setTotalCount] = useState();
+  const { url } = convertName(type);
+  useEffect(() => {
+    const dataFetch = async () => {
+      const response = await axios.get(url);
+      let comments, totalCount, article, articles, products, articleComments;
+      switch (type) {
+        case "articleWithComments":
+          ({ article, articleComments } = response.data);
+          ({ comments, totalCount } = articleComments);
+          setData(article);
+          setDataList(comments);
+          setTotalCount(totalCount);
+          break;
+      }
+    };
+    dataFetch();
+  }, [url, count, order, page, keyword, type]);
+  if (type === "articleWithComments") {
+    return {
+      article: data,
+      setArticle: setData,
+      articleComments: dataList,
+      setArticleComments: setDataList,
+      ArticleCommentsTotalCount: totalCount
+    };
+  }
+
+  return { data, setData };
+}
