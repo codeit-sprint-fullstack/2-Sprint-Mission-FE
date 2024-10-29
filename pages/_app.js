28895b2a133ea29f89f6f82bbfbeb31c1baa7cf9
Seo-Yurim <yurim2204@naver.com>
2024-10-29 15:35:12 +0900

    Refactor: 로컬 폰트 추가

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,13 @@ import Head from 'next/head';
 import Nav from '@/components/Nav/Nav';
 import Footer from '@/components/Footer/Footer';
 import Container from '@/components/Container';
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920'
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,11 +18,13 @@ export default function App({ Component, pageProps }) {
         <title>판다마켓</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Nav />
-      <Container>
-        <Component {...pageProps} />
-      </Container>
-      <Footer />
+      <div className={pretendard.className}>
+        <Nav />
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+        <Footer />
+      </div>
     </>
   );
 }
