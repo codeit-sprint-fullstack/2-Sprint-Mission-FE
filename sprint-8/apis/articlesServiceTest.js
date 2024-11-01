ee0d2c733f35cb1be18f0ca84e82d6cb2c830421
kipid <kipacti@gmail.com>
2024-11-01 17:40:52 +0900

    Delete sprint8

--- sprint-8/apis/articlesServiceTest.js
+++
@@ -1,8 +0,0 @@
-import { getArticles } from "./articlesService.js";
-
-async function getArticlesTest() {
-  const articles = await getArticles();
-  console.log(articles);
-}
-
-getArticlesTest();
--- sprint-8/pages/404.jsx
+++
@@ -1,5 +0,0 @@
-function NotFound() {
-	return <h1>Page is not found.</h1>
-}
-
-export default NotFound;
