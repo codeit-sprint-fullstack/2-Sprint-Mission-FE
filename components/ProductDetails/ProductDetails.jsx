ffd8cb2e2a0f11f10781c6a426f4b9ebf62f1821
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-04 11:49:56 +0900

    feat: product detail ui update

--- components/ProductDetails/ProductDetails.jsx
+++ components/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,11 @@
 import styles from "./ProductDetails.module.css";
 import { fetchApi } from "@/utils/axiosInstance";
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import product_default from "@/images/etc/product_default.svg";
+import selectImage from "@/images/board/select_img.svg";
+import user from "@/images/board/profile_img.svg";
+import heart_img from "@/images/board/heart_img.svg";
 
 const fetchProductDetail = async (id) => {
   if (id) {
@@ -12,6 +17,17 @@ const fetchProductDetail = async (id) => {
       console.error(error);
     }
   }
+};
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+  const formattedDate = date.toLocaleDateString("ko-KR", options);
+
+  return formattedDate
+    .replace(/\//g, " .")
+    .replace(/\s/g, " ")
+    .replace(/\.$/, "");
 };
 
 export default function ProductDetails({ productId }) {
@@ -32,7 +48,53 @@ export default function ProductDetails({ productId }) {
       <div className={styles.product_detail_container}>
         {product ? (
           <>
-            <h2>{product.name}</h2>
+            <Image src={product_default} />
+            <div className={styles.product_wrapper}>
+              <div className={styles.product_title}>
+                <h2>{product.name}</h2>
+                <div>
+                  <Image src={selectImage} />
+                </div>
+              </div>
+              <h1>{product.price.toLocaleString()}원</h1>
+              <div className={styles.divider_row}> </div>
+              <div className={styles.product_inform}>
+                <h5>상품소개</h5>
+                <p>{product.description}</p>
+                <h5>상품 태그</h5>
+                <div className={styles.tags_container}>
+                  {product.tags.map((tag, index) => (
+                    <div key={index} className={styles.tag_box}>
+                      #{tag}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className={styles.user_inform}>
+                <div className={styles.user_inform_wrapper}>
+                  <Image src={user} width={40} height={40} alt="유저 이미지" />
+                  <div className={styles.user_name}>
+                    <h5>{product.ownerNickname}</h5>
+                    <p>{formatDate(product.createdAt)}</p>
+                  </div>
+                </div>
+
+                <div className={styles.like_container}>
+                  <div className={styles.divider_col}></div>
+                  <div className={styles.like_wrapper}>
+                    <Image
+                      src={heart_img}
+                      width={32}
+                      height={32}
+                      alt="하트 이미지"
+                    />
+                    <div className={styles.like_count}>
+                      {product.favoriteCount}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </>
         ) : (
           <p>상품이 없습니다</p>
--- pages/items/[id].js
+++ pages/items/[id].js
@@ -1,13 +1,43 @@
+import CommentForm from "@/components/Comment/CommentForm";
 import ProductDetails from "@/components/ProductDetails/ProductDetails";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Market() {
   const router = useRouter();
   const { id } = router.query;
+
+  const [comment, setComment] = useState([]);
+  const [newComment, setNewComment] = useState("");
+
+  const handleCommentChange = (e) => setNewComment(e.target.value);
+
+  const handleCommentSubmit = async () => {
+    if (newComment.trim() === "") return;
+    try {
+      await fetchApi(
+        `/articles/${id}/comments`,
+        {
+          content: newComment,
+        },
+        "POST"
+      );
+      setNewComment("");
+      const updatedComments = await fetchCommentData(id);
+      setComment(updatedComments);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <h1>상세 페이지</h1>
       <ProductDetails productId={id} />
+      {/* <CommentForm */}
+      {/* // newComment={newComment} */}
+      {/* // onCommentChange={handleCommentChange} */}
+      {/* // onCommentSubmit={handleCommentSubmit} */}
+      {/* // /> */}
     </>
   );
 }
