f2dd2383c96f7280479f231aa86b5a7a1220558c
annylee0723 <annylee0723@gmail.com>
2024-11-04 16:26:09 +0900

    feat: create login page UI

--- sprint9/src/app/login/page.tsx
+++ sprint9/src/app/login/page.tsx
@@ -1,27 +1,34 @@
 "use client";
-//NOTE: onClick 사용하면 클라이언트 컴포넌트
+//NOTE: onClick > 클라이언트 컴포넌트
 
+import Image from "next/image";
+import Link from "next/link";
 import SignButton from "@/src/components/signInUp/SignButton";
 import SignInput from "@/src/components/signInUp/SignInput";
 import SocialMediaLogin from "@/src/components/signInUp/SocialMediaLogin";
-import Image from "next/image";
-import Link from "next/link";
-import logo from "../../../public/assets/pndamarket_logo.png";
+import logo from "@/public/assets/img_pandamarket_logo_login.png";
+import style from "@/src/styles/login.module.css";
 
 export default function LoginPage() {
   const handleClick = () => {};
 
   return (
-    <div>
-      <Image src={logo} alt="panda market logo" />
-      <SignInput value="email" />
-      <SignInput value="password" />
-      <SignButton status={false} onClick={handleClick}>
-        로그인
-      </SignButton>
-      <SocialMediaLogin />
-      <p> 판다마켓이 처음이신가요?</p>
-      <Link href="/sign-up">회원가입</Link>
+    <div className={style.container}>
+      <Image src={logo} width={396} height={132} alt="panda market logo" />
+      <div className={style.contentContainer}>
+        <SignInput value="email" />
+        <SignInput value="password" />
+        <SignButton status={false} onClick={handleClick}>
+          로그인
+        </SignButton>
+        <SocialMediaLogin />
+        <div className={style.signupContainer}>
+          <p className={style.signupText}> 판다마켓이 처음이신가요?</p>
+          <Link className={style.signupLink} href="/sign-up">
+            회원가입
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
