55f1fd62a1b01968f19b60c9ac36c3df3b851f42
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-04 14:44:52 +0900

    feat: like count update

--- components/ProductDetails/ProductDetails.jsx
+++ components/ProductDetails/ProductDetails.jsx
@@ -6,12 +6,13 @@ import product_default from "@/images/etc/product_default.svg";
 import selectImage from "@/images/board/select_img.svg";
 import user from "@/images/board/profile_img.svg";
 import heart_img from "@/images/board/heart_img.svg";
+import EditDeleteModal from "../EditDeleteModal/EditDeleteModal";
+import Popup from "../Popup/Popup";
 
 const fetchProductDetail = async (id) => {
   if (id) {
     try {
-      const data = await fetchApi(`/products/${id}`);
-      console.log(data);
+      const data = await fetchApi(`/products/${id}`, {}, "GET", true);
       return data;
     } catch (error) {
       console.error(error);
@@ -32,6 +33,14 @@ const formatDate = (dateString) => {
 
 export default function ProductDetails({ productId }) {
   const [product, setProduct] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+  const [isProductModal, setIsProductModal] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState(null);
+  const [editProductId, setEditProductId] = useState(null);
+  const [editProduct, setEditProduct] = useState("");
+  const [popupMessage, setPopupMessage] = useState("");
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   useEffect(() => {
     const getProductDetail = async () => {
@@ -43,17 +52,84 @@ export default function ProductDetails({ productId }) {
     }
   }, [productId]);
 
+  const toggleModal = (e, isProduct = false, productId = null) => {
+    const rect = e.target.getBoundingClientRect();
+    setModalPosition({ top: rect.top + 25, left: rect.left - 130 });
+    setIsModalOpen((prev) => !prev);
+    setIsProductModal(isProduct);
+    setSelectedProductId(productId);
+  };
+
+  const handleEditClick = () => {
+    setIsModalOpen(false);
+
+    if (isProductModal && selectedProductId) {
+      setEditProductId(selectedProductId);
+      if (product && product.id === selectedProductId) {
+        setEditProduct(product.content);
+      }
+    } else {
+      // router.push(`/products/edit/${id}`);
+    }
+  };
+
+  const handleDeleteClick = async () => {
+    try {
+      if (isProductModal && selectedProductId) {
+        await fetchApi(`/products/${selectedProductId}`, null, "DELETE");
+        router.push("/items");
+      } else {
+        await fetchApi(`/products/${productId}`, null, "DELETE");
+        router.push("/items");
+      }
+    } catch (e) {
+      const errorMessage = e.response?.data?.message;
+      setPopupMessage(errorMessage);
+      setIsPopupOpen(true);
+    }
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  const handleFavoriteToggle = async () => {
+    if (!product) return;
+    const newFavoriteState = !product.isFavorite;
+    try {
+      if (newFavoriteState) {
+        await fetchApi(`/products/${productId}/favorite`, {}, "POST", true); // 좋아요 추가
+      } else {
+        await fetchApi(`/products/${productId}/favorite`, {}, "DELETE", true); // 좋아요 취소
+      }
+      setProduct((prev) => ({
+        ...prev,
+        isFavorite: newFavoriteState,
+        favoriteCount: newFavoriteState
+          ? prev.favoriteCount + 1
+          : prev.favoriteCount - 1,
+      }));
+    } catch (error) {
+      console.error(error.response?.data || error.message);
+    }
+  };
+
   return (
     <>
       <div className={styles.product_detail_container}>
         {product ? (
           <>
-            <Image src={product_default} />
+            <Image src={product_default} alt="상품 기본 이미지" />
             <div className={styles.product_wrapper}>
               <div className={styles.product_title}>
                 <h2>{product.name}</h2>
                 <div>
-                  <Image src={selectImage} />
+                  <Image
+                    src={selectImage}
+                    alt="선택"
+                    onClick={(e) => toggleModal(e, true, product.id)}
+                    className={styles.select_button}
+                  />
                 </div>
               </div>
               <h1>{product.price.toLocaleString()}원</h1>
@@ -87,6 +163,11 @@ export default function ProductDetails({ productId }) {
                       width={32}
                       height={32}
                       alt="하트 이미지"
+                      onClick={handleFavoriteToggle}
+                      style={{
+                        cursor: "pointer",
+                        opacity: product.isFavorite ? 1 : 0.5,
+                      }}
                     />
                     <div className={styles.like_count}>
                       {product.favoriteCount}
@@ -100,6 +181,18 @@ export default function ProductDetails({ productId }) {
           <p>상품이 없습니다</p>
         )}
       </div>
+      {isModalOpen && (
+        <EditDeleteModal
+          onEdit={handleEditClick}
+          onDelete={handleDeleteClick}
+          style={{
+            position: "absolute",
+            top: `${modalPosition.top}px`,
+            left: `${modalPosition.left}px`,
+          }}
+        />
+      )}
+      {isPopupOpen && <Popup message={popupMessage} onClose={closePopup} />}
     </>
   );
 }
--- pages/items/[id].js
+++ pages/items/[id].js
@@ -1,7 +1,25 @@
 import CommentForm from "@/components/Comment/CommentForm";
 import ProductDetails from "@/components/ProductDetails/ProductDetails";
+import CommentItem from "@/components/Comment/CommentItem";
+import EditDeleteModal from "@/components/EditDeleteModal/EditDeleteModal";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import empty_img from "@/images/etc/img_default.svg";
+import { fetchApi } from "@/utils/axiosInstance";
+import styles from "./market.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const fetchCommentData = async (id) => {
+  if (id) {
+    try {
+      const data = await fetchApi(`/products/${id}/comments?limit=10`);
+      return Array.isArray(data) ? data : [];
+    } catch (error) {
+      return [];
+    }
+  }
+};
 
 export default function Market() {
   const router = useRouter();
@@ -9,6 +27,37 @@ export default function Market() {
 
   const [comment, setComment] = useState([]);
   const [newComment, setNewComment] = useState("");
+  const [editCommentId, setEditCommentId] = useState(null);
+  const [editComment, setEditComment] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCommentId, setSelectedCommentId] = useState(null);
+  const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      if (id) {
+        const fetchedComments = await fetchCommentData(id);
+        setComment(fetchedComments);
+      }
+    };
+    fetchComments();
+  }, [id]);
+
+  const handleDeleteComment = async () => {
+    try {
+      await fetchApi(
+        `/products/${id}/comments/${selectedCommentId}`,
+        null,
+        "DELETE"
+      );
+      const updatedComments = await fetchCommentData(id);
+      setComment(updatedComments);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.error("댓글 삭제 중 오류:", error);
+      alert("댓글 삭제 중 오류가 발생했습니다. 다시 시도해 주세요.");
+    }
+  };
 
   const handleCommentChange = (e) => setNewComment(e.target.value);
 
@@ -16,10 +65,8 @@ export default function Market() {
     if (newComment.trim() === "") return;
     try {
       await fetchApi(
-        `/articles/${id}/comments`,
-        {
-          content: newComment,
-        },
+        `/products/${id}/comments`,
+        { content: newComment },
         "POST"
       );
       setNewComment("");
@@ -30,14 +77,82 @@ export default function Market() {
     }
   };
 
+  const handleEditCommentChange = (e) => setEditComment(e.target.value);
+
+  const handleEditCommentSubmit = async () => {
+    if (editComment.trim() === "") return;
+    try {
+      await fetchApi(
+        `/products/${id}/comments/${editCommentId}`,
+        { content: editComment },
+        "POST"
+      );
+      setEditCommentId(null);
+      setEditComment("");
+      const updatedComments = await fetchCommentData(id);
+      setComment(updatedComments);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const toggleModal = (e, commentId) => {
+    const rect = e.target.getBoundingClientRect();
+    setModalPosition({ top: rect.top + 25, left: rect.left - 130 });
+    setIsModalOpen(!isModalOpen);
+    setSelectedCommentId(commentId);
+  };
+
   return (
     <>
       <ProductDetails productId={id} />
-      {/* <CommentForm */}
-      {/* // newComment={newComment} */}
-      {/* // onCommentChange={handleCommentChange} */}
-      {/* // onCommentSubmit={handleCommentSubmit} */}
-      {/* // /> */}
+      <CommentForm
+        newComment={newComment}
+        onCommentChange={handleCommentChange}
+        onCommentSubmit={handleCommentSubmit}
+      />
+
+      <div className="comment_container">
+        {comment.length === 0 ? (
+          <div className="empty_comment">
+            <Image src={empty_img} alt="빈 이미지" />
+            <p>아직 댓글이 없어요,</p>
+            <p>지금 댓글을 달아보세요!</p>
+          </div>
+        ) : (
+          comment.map((comment) => (
+            <CommentItem
+              key={comment.id}
+              comment={comment}
+              editCommentId={editCommentId}
+              editComment={editComment}
+              handleEditCommentChange={handleEditCommentChange}
+              handleEditCommentSubmit={handleEditCommentSubmit}
+              toggleModal={toggleModal}
+              userImage={user}
+              selectImage={selectImage}
+            />
+          ))
+        )}
+      </div>
+      <div className={styles.return_button_container}>
+        <Link href="/items">
+          <button className={styles.return_list}>목록으로 돌아가기</button>
+        </Link>
+      </div>
+      {isModalOpen && (
+        <EditDeleteModal
+          onDelete={handleDeleteComment}
+          onEdit={() => {
+            setIsModalOpen(false);
+          }}
+          style={{
+            position: "absolute",
+            top: `${modalPosition.top}px`,
+            left: `${modalPosition.left}px`,
+          }}
+        />
+      )}
     </>
   );
 }
