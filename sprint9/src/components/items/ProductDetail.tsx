691c5ae452f43302db37870a8acbd5e26258cf31
annylee0723 <annylee0723@gmail.com>
2024-11-05 22:14:01 +0900

    feat: add Product Detail component

--- sprint9/src/components/items/ProductDetail.tsx
+++ sprint9/src/components/items/ProductDetail.tsx
@@ -1,37 +1,55 @@
-"use client";
-
 import Image from "next/image";
 import style from "@/src/styles/items/ProductDetail.module.css";
 import profileImg from "@/public/assets/img_profile.png";
 import heartIcon from "@/public/assets/icon_heart.png";
+import formatDate from "@/src/utils/formatDate";
 import DropBox from "../DropBox";
 
 interface ProductDetailProps {
-  data: object | null;
+  data: {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    tags: string[];
+    images: string[];
+    ownerId: number;
+    favoriteCount: number;
+    createdAt: string;
+    updatedAt: string;
+    ownerNickname: string;
+    isFavorite: boolean;
+  } | null;
 }
 
-//TODO: API 연결 후 수정하기, Dropbox props 전달
-export default function ProductDetail(data: object) {
+//TODO: Dropbox props 전달
+export default function ProductDetail({ data }: ProductDetailProps) {
+  if (!data) return null;
+
   return (
-    <div>
+    <div className={style.container}>
       <div>
-        <Image />
+        <Image
+          className={style.image}
+          src={data.images[0]}
+          alt={`${data.name} image`}
+          width={486}
+          height={486}
+        />
       </div>
       <div className={style.detailContainer}>
         <div className={style.titleContainer}>
           <div className={style.dropBoxContainer}>
-            <h2 className={style.title}>아이패드 미니 팔아요</h2>
+            <h2 className={style.title}>{data.name}</h2>
             <DropBox />
           </div>
-          <h1 className={style.price}>500,000원</h1>
+          <h1 className={style.price}>{`${data.price}원`}</h1>
         </div>
         <hr className={style.lineHorizontal} />
         <div className={style.bottomContainer}>
           <div className={style.descriptionContainer}>
             <p className={style.subtitle}>상품 소개</p>
-            <p className={style.description}>
-              액정이 잔기스랑 어쩌고 저쩌고 있습니다 와랄랄라 올롤롤로
-            </p>
+            <p className={style.description}>{data.description}</p>
           </div>
           <div className={style.descriptionContainer}>
             <p className={style.subtitle}>상품 태그</p>
@@ -45,14 +63,14 @@ export default function ProductDetail(data: object) {
           <div className={style.profileContainer}>
             <Image src={profileImg} alt="a white panda with grey background" />
             <div className={style.nicknameDataContainer}>
-              <p className={style.nickname}>총명한 판다</p>
-              <p className={style.date}>2022.01.11</p>
+              <p className={style.nickname}>{data.ownerNickname}</p>
+              <p className={style.date}>{formatDate(data.createdAt)}</p>
             </div>
           </div>
           <hr className={style.line} />
           <div className={style.likeContainer}>
             <Image src={heartIcon} alt="heart icon" />
-            <p className={style.likes}>123</p>
+            <p className={style.likes}>{data.favoriteCount}</p>
           </div>
         </div>
       </div>
