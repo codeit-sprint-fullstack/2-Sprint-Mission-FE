81ee8feb3096173da4de46fb21da87b55bb2a8d2
annylee0723 <annylee0723@gmail.com>
2024-11-05 22:24:32 +0900

    feat: add api endpoints

---
+++ sprint9/src/api/productServices.ts
@@ -0,0 +1,54 @@
+import { getRequest, postRequest, patchRequest, deleteRequest } from "./api";
+
+export async function getProducts({ order = "", searchKeyword = "" } = {}) {
+  try {
+    const response = await getRequest(`/articles`, {
+      order,
+      search: searchKeyword
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error on getting articles", error);
+    throw error;
+  }
+}
+
+export async function getProduct(id: string) {
+  try {
+    const response = await getRequest(`/products/${id}`, {});
+    return response.data;
+  } catch (error) {
+    console.error("Error on getting an article", error);
+    throw error;
+  }
+}
+
+export async function postArticle(data: object) {
+  try {
+    const response = await postRequest(`/articles`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error on posting an article:", error);
+    throw error;
+  }
+}
+
+export async function patchProduct(id: string, data: object) {
+  try {
+    const response = await patchRequest(`/products/${id}`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error on patching an article:", error);
+    throw error;
+  }
+}
+
+export async function deleteProduct(id: string) {
+  try {
+    const response = await deleteRequest(`/products/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error on removing an article:", error);
+    throw error;
+  }
+}
