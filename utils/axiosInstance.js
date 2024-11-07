466b0d6b84570273abb57649df540005e854b924
Yang ga-hyeon <didrkgus0426@gmail.com>
2024-11-07 10:52:51 +0900

    fix: api address move to env

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://panda-market-api.vercel.app",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 5000,
 });
 
