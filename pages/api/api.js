a6f9ba4d11d343f5c42957c69f71447a7bbea934
shinji530 <shinji53059@gmail.com>
2024-11-09 11:21:06 +0900

    Feat: initial commit

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://pandamarket-3uzp.onrender.com",
+  baseURL: "https://panda-market-api.vercel.app",
+  // baseURL: "https://pandamarket-3uzp.onrender.com",
   // baseURL: "http://localhost:3001",
   // timeout: 10000,
 });
