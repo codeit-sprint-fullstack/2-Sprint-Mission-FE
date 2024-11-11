a39730ec634a9c5694c2503167c2971ae82afc08
hyobiin9 <moone1234@kakao.com>
2024-11-11 09:47:47 +0900

    feat: signup ui

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,22 +1,29 @@
 import Image from "next/image";
-import Link from "next/link";
 import pandaLogo from "../../../public/panda-logo.svg";
 import google from "../../../public/ic_google.svg";
 import kakao from "../../../public/ic_kakao.svg";
 import eye from "../../../public/btn_visibility_on.png";
-import styles from "../../styles/Login.module.css";
+import styles from "../../styles/Signup.module.css";
+import Link from "next/link";
 
-export default function Login() {
+export default function Signup() {
   return (
-    <div className={styles.login}>
+    <div className={styles.signup}>
       <Image className={styles.panda_image} src={pandaLogo} alt="판다마켓" />
-      <div className={styles.login_bottom}>
+      <div className={styles.signup_bottom}>
         <div className={styles.input_wrapper}>
           <div className={styles.wrapper}>
             <p className={styles.text}>이메일</p>
             <input
               className={styles.email_input}
               placeholder="이메일을 입력해주세요"
+            />
+          </div>
+          <div className={styles.wrapper}>
+            <p className={styles.text}>닉네임</p>
+            <input
+              className={styles.nick_input}
+              placeholder="닉네임을 입력해주세요"
             />
           </div>
           <div className={styles.wrapper}>
@@ -28,9 +35,19 @@ export default function Login() {
               />
               <Image src={eye} alt="눈" />
             </div>
+          </div>{" "}
+          <div className={styles.wrapper}>
+            <p className={styles.text}>비밀번호 확인</p>
+            <div className={styles.eye_wrapper}>
+              <input
+                className={styles.pw_input}
+                placeholder="비밀번호를 다시 한 번 입력해주세요"
+              />
+              <Image src={eye} alt="눈" />
+            </div>
           </div>
         </div>
-        <button className={styles.login_button}>로그인</button>
+        <button className={styles.signup_button}>회원가입</button>
         <div className={styles.simple_login}>
           <p className={styles.simple_login_text}>간편 로그인하기</p>
           <div className={styles.icons}>
@@ -39,9 +56,9 @@ export default function Login() {
           </div>
         </div>
         <div className={styles.first}>
-          <p className={styles.first_text}>판다마켓이 처음이신가요?</p>
-          <Link href="/signup" className={styles.signup}>
-            회원가입
+          <p className={styles.first_text}>이미 회원이신가요?</p>
+          <Link href="/signup" className={styles.login}>
+            로그인
           </Link>
         </div>
       </div>
