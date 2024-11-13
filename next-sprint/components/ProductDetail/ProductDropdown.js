822d29d667cf6bbad31757fbf20de4d0d0954f1a
unknown <d653@naver.com>
2024-11-13 11:28:11 +0900

    Feat: 상품 수정 & 삭제 모달

--- next-sprint/components/ProductDetail/ProductDropdown.js
+++ next-sprint/components/ProductDetail/ProductDropdown.js
@@ -1,3 +1,21 @@
-export default function ProductDropdown() {
-  return;
+import styles from './ProductDropdown.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function ProductDropdown({ onDeleteClick }) {
+  const router = useRouter();
+  const id = router.query['id'];
+
+  return (
+    <div className={styles.menu}>
+      <div className={styles[`patch-menu`]}>
+        <Link className={styles.Link} href={`/items/${id}/edit`}>
+          수정하기
+        </Link>
+      </div>
+      <div className={styles[`delete-menu`]} onClick={() => onDeleteClick(id)}>
+        삭제하기
+      </div>
+    </div>
+  );
 }
