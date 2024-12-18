bec4f549e3f28aa9a01c69b426208f6ba5aa919b
hyobiin9 <moone1234@kakao.com>
2024-12-18 18:51:22 +0900

    feat: login page route

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/../public/assets/header_logo.svg";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleClickEvent = () => {
+    router.push("/signin");
+  };
+
   return (
     <div className="w-full py-[0.9rem] px-[20rem] gap-[1rem] border border-b-[#DFDFDF]">
       <div className="w-full flex justify-between items-center">
         <Image src={logo} alt="logo" />
-        <button className="w-[12.8rem] h-[4.8rem] rounded-[0.8rem] py-[1.2rem] px-[2.3rem] flex items-center justify-center gap-[1rem] bg-[#3692FF] font-semibold text-[1.6rem] leading-[2.6rem] text-[#F3F4F6]">
+        <button
+          className="w-[12.8rem] h-[4.8rem] rounded-[0.8rem] py-[1.2rem] px-[2.3rem] flex items-center justify-center gap-[1rem] bg-[#3692FF] font-semibold text-[1.6rem] leading-[2.6rem] text-[#F3F4F6]"
+          onClick={handleClickEvent}
+        >
           로그인
         </button>
       </div>
