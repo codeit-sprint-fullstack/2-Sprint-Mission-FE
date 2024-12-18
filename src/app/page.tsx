609b1fccf69afd54b0482f05d1fa989bc3f7e9ff
hyobiin9 <moone1234@kakao.com>
2024-12-18 18:47:44 +0900

    feat: landing card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import home_top from "@/../public/assets/Img_home_top.svg";
 import home_bottom from "@/../public/assets/Img_home_bottom.svg";
+import LandingCard from "@/components/landingCard";
+import home1 from "@/../public/assets/Img_home_01.svg";
+import home2 from "@/../public/assets/Img_home_02.svg";
+import home3 from "@/../public/assets/Img_home_03.svg";
 
 export default function Home() {
   return (
@@ -19,8 +23,42 @@ export default function Home() {
         </div>
       </div>
       <div>
-        <div className="w-full py-[13.8rem] px-[34.4rem]">
-          <div></div>
+        <div className="w-full py-[13.8rem] flex items-center justify-center">
+          <LandingCard
+            image={home1}
+            category="Hot item"
+            title1="인기 상품을"
+            title2="확인해 보세요"
+            description1="가장 HOT한 중고거래 물품을"
+            description2="판다 마켓에서 확인해 보세요"
+            align="start"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="w-full py-[13.8rem] flex items-center justify-center">
+          <LandingCard
+            image={home2}
+            category="Search"
+            title1="구매를 원하는"
+            title2="상품을 검색하세요"
+            description1="구매하고 싶은 물품은 검색해서"
+            description2="쉽게 찾아보세요"
+            align="end"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="w-full py-[13.8rem] flex items-center justify-center">
+          <LandingCard
+            image={home3}
+            category="Register"
+            title1="판매를 원하는"
+            title2="상품을 등록하세요"
+            description1="어떤 물건이든 판매하고 싶은 상품을"
+            description2="쉽게 등록하세요"
+            align="start"
+          />
         </div>
       </div>
       <div className="w-full bg-[#CFE5FF] flex justify-center items-end h-[54rem]">
@@ -30,7 +68,7 @@ export default function Home() {
               믿을 수 있는 <br /> 판다마켓 중고 거래
             </p>
           </div>
-          <Image src={home_bottom} alt="home-top" width={746} height={397} />
+          <Image src={home_bottom} alt="home-bottom" width={746} height={397} />
         </div>
       </div>
     </div>
--- src/components/landingCard.tsx
+++ src/components/landingCard.tsx
@@ -1,7 +1,77 @@
-export default function LandingCard() {
+import Image from "next/image";
+
+interface landingCardProps {
+  image: string;
+  category: string;
+  title1: string;
+  title2: string;
+  description1: string;
+  description2: string;
+  align: string;
+}
+
+export default function LandingCard({
+  image,
+  category,
+  title1,
+  title2,
+  description1,
+  description2,
+  align,
+}: landingCardProps) {
   return (
-    <div>
-      <div></div>
+    <div
+      className={`w-[98.8rem] gap-[6.4rem] bg-[#FCFCFC] flex ${
+        align === "start" ? "justify-start" : "justify-end"
+      }`}
+    >
+      {align === "start" && (
+        <Image src={image} alt="image" width={588} height={444} />
+      )}
+      <div className="flex flex-col gap-[1.2rem] items-center justify-center">
+        <div
+          className={`flex flex-col ${
+            align === "start" ? "items-start" : "items-end"
+          }`}
+        >
+          <p className="font-bold text-[1.8rem] leading-[2.6rem] text-[#3692FF]">
+            {category}
+          </p>
+          <div
+            className={`flex flex-col gap-[2.4rem] ${
+              align === "start" ? "items-start" : "items-end"
+            }`}
+          >
+            <div
+              className={`flex flex-col ${
+                align === "start" ? "items-start" : "items-end"
+              }`}
+            >
+              <p className="font-bold text-[4rem] leading-[5.6rem] text-[#374151]">
+                {title1}
+              </p>
+              <p className="font-bold text-[4rem] leading-[5.6rem] text-[#374151]">
+                {title2}
+              </p>
+            </div>
+            <div
+              className={`flex flex-col ${
+                align === "start" ? "items-start" : "items-end"
+              }`}
+            >
+              <p className="font-medium text-[2.4rem] leading-[3.2rem]">
+                {description1}
+              </p>
+              <p className="font-medium text-[2.4rem] leading-[3.2rem]">
+                {description2}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {align === "end" && (
+        <Image src={image} alt="image" width={588} height={444} />
+      )}
     </div>
   );
 }
